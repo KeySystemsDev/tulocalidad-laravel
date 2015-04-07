87ba21f527328a2aa956cea7eee79462ec8ede3e
vinrast <vinrast@gmail.com>
2015-04-07 11:07:37 -0430

    cambios en ruta

--- app/Http/Controllers/RegistroController.php
+++ app/Http/Controllers/RegistroController.php
@@ -10,64 +10,35 @@ class RegistroController extends Controller {
 	public function registro_empresa(){
 
 	
-	return View::make('Registro/Registro_empresa/f_empresa');   
+	return View::make('Registro/Registro_empresa/formulario_empresa');   
 }
 
 
 	public function procesado_empresa(){
 		$data =input::all();
-		/*echo $data[$nombre];
-		echo $data[$rif];
-		echo $data[$direccion];
-		echo $data[$categoria];
-		echo $data[$estados];
-		echo $data[$telefono];
-		echo $data[$telefono2];
-		echo $data[$telefono3];
-		echo $data[$celular];*/
 
 	 return View::make('Registro/Registro_empresa/procesado', compact('data'));   
 }
 	public function registro_publicidad(){
 
 	
-	return View::make('Registro/Registro_publicidad/f_publicidad');   
+	return View::make('Registro/Registro_publicidad/formulario_publicidad');   
 }
-
 
 	public function procesado_publicidad(){
 		$data =input::all();
-		/*echo $data[$nombre];
-		echo $data[$rif];
-		echo $data[$direccion];
-		echo $data[$categoria];
-		echo $data[$estados];
-		echo $data[$telefono];
-		echo $data[$telefono2];
-		echo $data[$telefono3];
-		echo $data[$celular];*/
 
 	 return View::make('Registro/Registro_publicidad/procesado',compact('data'));   
 }
 public function registro_usuario(){
 
 	
-	return View::make('Registro/Registro_usuario/f_usuario');   
+	return View::make('Registro/Registro_usuario/formulario_usuario');   
 }
-
 
 	public function procesado_usuario(){
 		$data =input::all();
-		/*echo $data[$nombre];
-		echo $data[$rif];
-		echo $data[$direccion];
-		echo $data[$categoria];
-		echo $data[$estados];
-		echo $data[$telefono];
-		echo $data[$telefono2];
-		echo $data[$telefono3];
-		echo $data[$celular];*/
-
+		
 	 return View::make('Registro/Registro_usuario/procesado',compact('data'));   
 }
 }
--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,23 +11,23 @@
 |
 */
 
-Route::get('/', 'WelcomeController@index');
+/*Route::get('/', 'WelcomeController@index');
 
 Route::get('home', 'HomeController@index');
 
 Route::controllers([
 	'auth' => 'Auth\AuthController',
 	'password' => 'Auth\PasswordController',
-]);
+]);*/
 
 Route::get('ejemplo', 'EjemploController@index');
 Route::get('ejemplo/nueva/{id}', 'EjemploController@nueva');
 Route::get('ejemplo/siguiente/{id}', 'EjemploController@siguiente');
 Route::get('formulario', 'formularioController@index');
 Route::post('formulario/recibir', 'formularioController@registro');
-Route::get('registro/registro_empresa/f_empresa', 'RegistroController@registro_empresa');
-Route::post('registro/registro_empresa/procesado', 'RegistroController@procesado_empresa');
-Route::get('registro/registro_publicidad/f_publicidad', 'RegistroController@registro_publicidad');
-Route::post('registro/registro_publicidad/procesado', 'RegistroController@procesado_publicidad');
-Route::get('registro/registro_usuario/f_usuario', 'RegistroController@registro_usuario');
-Route::post('registro/registro_usuario/procesado', 'RegistroController@procesado_usuario');
+Route::get('registro/registro_empresa/formulario_empresa', 'RegistroController@registro_empresa');
+/*Route::post('registro/registro_empresa/', 'RegistroController@procesado_empresa');*/
+Route::get('registro/registro_publicidad/', 'RegistroController@registro_publicidad');
+Route::post('registro/registro_publicidad/', 'RegistroController@procesado_publicidad');
+Route::get('registro/registro_usuario/', 'RegistroController@registro_usuario');
+Route::post('registro/registro_usuario/', 'RegistroController@procesado_usuario');

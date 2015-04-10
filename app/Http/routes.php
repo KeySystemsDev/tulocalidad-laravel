db1f0101988d75f1b31a5f2d02ca5f578484161d
vinrast <vinrast@gmail.com>
2015-04-10 17:38:14 -0430

    Cambios en las rutas
    Se modifico y borro las rutas que no estaban en uso y se creo un unico
    controlador y una unica vista para realizar pruebas

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -12,19 +12,6 @@
 */
 
 Route::get('/', 'WelcomeController@index');
-
-Route::get('home', 'HomeController@index');
-
-Route::controllers([
-	'auth' => 'Auth\AuthController',
-	'password' => 'Auth\PasswordController',
-]);
-
-Route::get('ejemplo', 'EjemploController@index');
-Route::get('ejemplo/nueva/{id}', 'EjemploController@nueva');
-Route::get('ejemplo/siguiente/{id}', 'EjemploController@siguiente');
-Route::get('formulario', 'formularioController@index');
-Route::post('formulario/recibir', 'formularioController@registro');
 Route::get('registro/empresa-registro/empresa-formulario', 'RegistroController@empresa_registro');
 Route::post('registro/empresa-registro/procesado', 'RegistroController@empresa_procesado');
 Route::get('registro/publicidad-registro/publicidad-formulario', 'RegistroController@publicidad_registro');

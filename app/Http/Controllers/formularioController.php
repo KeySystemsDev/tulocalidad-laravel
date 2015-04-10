bf10d49ebdad42130e322832f6224f4f0a29dc82
vinrast <vinrast@gmail.com>
2015-04-09 21:44:20 -0430

    pruebas con modelos

--- app/Http/Controllers/formularioController.php
+++ app/Http/Controllers/formularioController.php
@@ -2,7 +2,10 @@
 use Illuminate\Support\Facades\Input;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
-use View;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Auth\Authenticatable;
+use App\ModelApp\Empresa;
+use Illuminate\Http\Request;
 //use Input;
 
 class formularioController extends Controller {
@@ -15,10 +18,22 @@ class formularioController extends Controller {
 
 
 	public function registro(){
-		$data =input::all();
+		$data = input::all();
+		$direccion = Input::get('direccion');
+		echo $direccion;
+		/*$data = input::direccion
+		print_r($data);*/
 
-	 return View::make('recibir',compact('data'));   
+
+	 //return View::make('recibir',compact('data'));   
 }
+
+	public function conectar(){
+		/*$empresa = new Empresa;
+		$resultado = $empresa -> empresa_consulta();
+		print_r($resultado);*/
+		$empresa = Empresa::empresa_consulta()->get();
 }
+	}
 
 ?>

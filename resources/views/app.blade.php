4249b8d77f620e097d4d2a76338f6233742958cf
Carlos Espinoza <carlosarmikhael@gmail.com>
2015-04-15 00:22:55 -0430

    lib animate y trans z-index nav

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -21,12 +21,13 @@
 		<!-- CSS 
 	        ================================================== -->
 	    <!-- Bootstrap 3-->
-	    <link href="{{ asset('/css/bootstrap.min.css') }}" rel="stylesheet" media="screen">
+	    <link href="/bower_components/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet" media="screen">
 	    <!-- Google Fonts -->
 	    <link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro:200,300,400,600,700,900,200italic,300italic,400italic,600italic,700italic,900italic|Roboto+Condensed:300italic,400italic,700italic,400,300,700' rel='stylesheet' type='text/css'>
 	    <link href='http://fonts.googleapis.com/css?family=Roboto+Condensed:300italic,400italic,700italic,400,300,700' rel='stylesheet' type='text/css'>
 	    <!-- Template Styles -->
 	    <link href="{{ asset('/css/style.css') }}" rel="stylesheet" media="screen">
+	    <link rel="stylesheet" type="text/css" href="/bower_components/animate.css/animate.min.css">
 	    <link rel="shortcut icon" href="favicon.ico" />
 
 	</head>
@@ -38,11 +39,13 @@
 		<!-- Scripts -->
 			 <!-- JAVASCRIPT
 		     ================================================== -->
-	    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
-	    <script src="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.1/js/bootstrap.min.js"></script>
-	    <script src="{{ asset('/js/animatescroll.js') }}"></script>
-	    <script src="{{ asset('/js/scripts.js') }}"></script>
-	    <script src="{{ asset('/js/retina.min.js') }}"></script>
+	    <script src="/bower_components/jquery/dist/jquery.min.js"></script>
+	    <script src="/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+	    <script src="/bower_components/animatescroll/animatescroll.min.js"></script>
+	    <script src="/bower_components/retinajs/dist/retina.min.js"></script>
+
+	    <!-- Scripts -->
+	    <script src="/js/scripts.js"></script>
 
 	</body>
 </html>

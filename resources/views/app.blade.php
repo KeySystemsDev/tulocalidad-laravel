21c7ce29156f4b2c4a6dad9d19ea383e4dac39ac
Carlos Espinoza <carlosarmikhael@gmail.com>
2015-04-17 01:29:22 -0430

    cambio en llamado de lib

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -21,13 +21,13 @@
 		<!-- CSS 
 	        ================================================== -->
 	    <!-- Bootstrap 3-->
-	    <link href="/bower_components/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet" media="screen">
+	    <link href="{{ asset('/bower_components/bootstrap/dist/css/bootstrap.min.css') }}" rel="stylesheet" media="screen">
 	    <!-- Google Fonts -->
 	    <link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro:200,300,400,600,700,900,200italic,300italic,400italic,600italic,700italic,900italic|Roboto+Condensed:300italic,400italic,700italic,400,300,700' rel='stylesheet' type='text/css'>
 	    <link href='http://fonts.googleapis.com/css?family=Roboto+Condensed:300italic,400italic,700italic,400,300,700' rel='stylesheet' type='text/css'>
 	    <!-- Template Styles -->
 	    <link href="{{ asset('/css/style.css') }}" rel="stylesheet" media="screen">
-	    <link rel="stylesheet" type="text/css" href="/bower_components/animate.css/animate.min.css">
+	    <link rel="stylesheet" type="text/css" href="{{ asset('/bower_components/animate.css/animate.min.css') }}">
 	    <link rel="shortcut icon" href="favicon.ico" />
 
 	</head>
@@ -39,13 +39,13 @@
 		<!-- Scripts -->
 			 <!-- JAVASCRIPT
 		     ================================================== -->
-	    <script src="/bower_components/jquery/dist/jquery.min.js"></script>
-	    <script src="/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
-	    <script src="/bower_components/animatescroll/animatescroll.min.js"></script>
-	    <script src="/bower_components/retinajs/dist/retina.min.js"></script>
+	    <script src="{{ asset('/bower_components/jquery/dist/jquery.min.js') }}"></script>
+	    <script src="{{ asset('/bower_components/bootstrap/dist/js/bootstrap.min.js') }}"></script>
+	    <script src="{{ asset('/bower_components/animatescroll/animatescroll.min.js') }}"></script>
+	    <script src="{{ asset('/bower_components/retinajs/dist/retina.min.js') }}"></script>
 
 	    <!-- Scripts -->
-	    <script src="/js/scripts.js"></script>
+	    <script src="{{ asset('/js/scripts.js') }}"></script>
 
 	</body>
 </html>

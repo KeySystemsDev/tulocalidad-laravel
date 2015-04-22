3e4fd4d3cf463e8459fe523f4c0c8734d7efac1e
Carlos Espinoza <carlosarmikhael@gmail.com>
2015-04-22 02:41:39 -0430

    mapa de google map angularp

--- public/js/controller.js
+++ public/js/controller.js
@@ -3,7 +3,40 @@
 
 angular.module('tulocalidad.controller', [])
 
-.controller('WelcomeController', function($scope) {
+.controller('WelcomeController', function($scope, $log) {
 	$log.log('WelcomeController');
+})
 
+.controller('EmpresaController', function($scope, $log) {
+	$log.log('EmpresaController');
+
+	$scope.map = {center: {latitude: 10.4713637669733, longitude: -66.807892578125 }, zoom: 9 };
+    $scope.map = {center: {latitude: 10.4713637669733, longitude: -66.807892578125 }, zoom: 9 };
+    $scope.options = {scrollwheel: false};
+    $scope.coordsUpdates = 0;
+    $scope.dynamicMoveCtr = 0;
+    $scope.marker = {
+      	id: 0,
+      	coords: {
+        	latitude: 10.4713637669733,
+        	longitude: -66.807892578125
+      	},
+      	options: { draggable: true },
+      	events: {
+        	dragend: function (marker, eventName, args) {
+          		$log.log('marker dragend');
+          		var lat = marker.getPosition().lat();
+          		var lon = marker.getPosition().lng();
+          		$log.log(lat);
+          		$log.log(lon);
+
+          	$scope.marker.options = {
+            	draggable: true,
+            	labelContent: "lat: " + $scope.marker.coords.latitude + ' ' + 'lon: ' + $scope.marker.coords.longitude,
+            	labelAnchor: "100 0",
+            	labelClass: "marker-labels"
+          };
+        }
+      }
+    };
 });
--- resources/views/registro/empresa_registro/empresa_registrar.blade.php
+++ resources/views/registro/empresa_registro/empresa_registrar.blade.php
@@ -1,21 +1,35 @@
-<h1>Formulario de Registro de Empresa</h1>
-<p>Registra tu empresa para el directorio</p>
-<form action ='empresa-procesado', method ='post', name ='formulario'>
-	Nombre:<input type ="text", maxlength="20", name ="i_nombre", value ="" ><br>
-	Rif:<input type ="text", maxlength="10", name ="i_rif", value ="" >
-	<br>	
-	Estados:
-	<select name="i_estados">
-		@foreach($estados as $value)
-			<option class="option" value={{ $value->id_estado }}> {{$value->nombre_estado}} </option>; 
-		@endforeach
-	</select>
-	<br> 	
-	Direccion:<input type ="text", maxlength="100", name ="i_direccion", value ="" ><br> 
-	Categorias:<select name="i_categoria">@foreach($categoria as $value)<option class="option" value={{$value->id_categoria}}>{{$value->nombre_categoria}}</option>; @endforeach</select><br>	
-	Telefono:<input type ="text", maxlength="11", name ="i_telefono", value ="" ><br>
-	Telefono 2:<input type ="text", maxlength="11", name ="i_telefono2", value ="" ><br>
-	Telefono 3:<input type ="text", maxlength="11", name ="i_telefono3", value ="" ><br>
-	Telefono movil:<input type ="text", maxlength="11", name ="i_celular", value ="" ><br>
-	<input type="submit" value="Registrar">
-</form>
+@extends('app')
+
+@section('content')
+<div ng-controller="EmpresaController">
+	
+	<h1>Formulario de Registro de Empresa</h1>
+	<p>Registra tu empresa para el directorio</p>
+	<form action ='empresa-procesado', method ='post', name ='formulario'>
+		Nombre:<input type ="text", maxlength="20", name ="i_nombre", value ="" ><br>
+		Rif:<input type ="text", maxlength="10", name ="i_rif", value ="" >
+		<br>	
+		Estados:
+		<select name="i_estados">
+			@foreach($estados as $value)
+				<option class="option" value={{ $value->id_estado }}> {{$value->nombre_estado}} </option>; 
+			@endforeach
+		</select>
+		<br> 	
+		Direccion:<input type ="text", maxlength="100", name ="i_direccion", value ="" ><br> 
+		Categorias:<select name="i_categoria">@foreach($categoria as $value)<option class="option" value={{$value->id_categoria}}>{{$value->nombre_categoria}}</option>; @endforeach</select><br>	
+		Telefono:<input type ="text", maxlength="11", name ="i_telefono", value ="" ><br>
+		Telefono 2:<input type ="text", maxlength="11", name ="i_telefono2", value ="" ><br>
+		Telefono 3:<input type ="text", maxlength="11", name ="i_telefono3", value ="" ><br>
+		Telefono movil:<input type ="text", maxlength="11", name ="i_celular", value ="" ><br>
+		<input type="submit" value="Registrar">
+	</form>
+
+	<div id="map_canvas">
+	    <ui-gmap-google-map center="map.center" zoom="map.zoom" draggable="true" options="options">
+	        <ui-gmap-marker coords="marker.coords" options="marker.options" events="marker.events" idkey="marker.id">
+	        </ui-gmap-marker>
+	    </ui-gmap-google-map>
+    </div>
+</div>
+@endsection

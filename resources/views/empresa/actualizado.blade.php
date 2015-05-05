a46bbf34203fb249229a5f58aa5f00ab6131c07f
Carlos Espinoza <carlosarmikhael@gmail.com>
2015-05-04 22:55:08 -0430

    vista actualizado estilo listo

--- resources/views/empresa/actualizado.blade.php
+++ resources/views/empresa/actualizado.blade.php
@@ -1,2 +1,42 @@
-<center><h1>Actualizado con exito</h1></center>
-<a href="../empresa/consulta?v={{$rif}}">Volver</a>
+@extends('base')
+
+@section('content')
+
+	@include('layouts/nav')
+	<div class="container">
+		<div id="main">
+			<div class="row">
+				 <div class="col-lg-12">
+	                    <section class="panel">                         
+	                        <div class="panel-body">
+
+		                        <div class="col-lg-12">
+				                	<section class="panel">
+				                		<header class="panel-heading center">
+						                    <img class="img-registrar-logo" src="{{ asset('/img/tulocalidad.png') }}">
+						                    <h2>
+						                        Actualizado con exito
+						                    </h2>
+				                    	</header>
+				                    </section>
+				                </div>
+
+				                <div class="col-lg-12">
+				                	<section class="panel">
+				                		<header class="panel-heading center">
+				            				<a href="../empresa/consulta?v={{$rif}}">
+				            					<button class="btn btn-danger btn-lg fa fa-arrow-left"> Volver</button>
+											</a>			      					
+				      					</header>
+				      				</section>
+			      				</div>
+
+
+							</div>
+						</section>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+@endsection

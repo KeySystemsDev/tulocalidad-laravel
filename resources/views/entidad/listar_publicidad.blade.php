db55befc2ec72cb4f4ecee3dd06b5c2c9f4c0c0f
vinrast <vinrast@gmail.com>
2015-06-29 12:42:46 -0430

    quitando boton de publicidad a listar-publicidad

--- resources/views/entidad/listar_publicidad.blade.php
+++ resources/views/entidad/listar_publicidad.blade.php
@@ -7,7 +7,6 @@
             <th>Rif</th>                                
             <th>Campaña</th>
             <th>Editar</th>
-            <th>Publicar</th> 
             <th>Deshabilitar</th>
         </tr>
     </thead>
@@ -16,7 +15,6 @@
             <td></td>
             <td></td>
             <td><a href=""><button class="btn btn-danger btn-xs">Editar</button></a></td>                                       
-            <td><a href=""><button class="btn btn-primary btn-xs" data-original-title="" title=""><i class="fa fa-pencil" data-original-title="" title=""></i>Publicidad</button></a></td> 
             <td><a href=""><button class="btn btn-danger btn-xs">Deshabilitar</button></a></td> 
         </tr>
     </tbody>

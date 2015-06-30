8d2bf13f3507cd21861aa11b6d11db968657c7bc
Diego Carciente <diego.carciente@gmail.com>
2015-06-29 22:35:53 -0430

    Agregando nuevas vistas de publicidad

---
+++ resources/views/publicidad/editar_publicidad.blade.php
@@ -0,0 +1,1 @@
+<h1>Agregar Publicidad --> {{ $id_publicidad}}</h1>
---
+++ resources/views/publicidad/mis_publicidades.blade.php
@@ -0,0 +1,22 @@
+    
+    <h1>Mis Publicidades</h1>
+    <br>
+    <a href="../mis-publicidades/agregar-publicidad">Agregar Publicidad</a>
+    <table class="table table-striped table-advance table-hover">
+        <thead>
+            <tr>
+                <th>Rif</th>                                
+                <th>Campaña</th>
+                <th>Editar</th>
+                <th>Deshabilitar</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>Rif</td>
+                <td>Campaña</td>
+                <td><a href="../mis-publicidades/editar-publicidad/1">Editar</a></td>                                       
+                <td><a href="">Deshabilitar</a></td> 
+            </tr>
+        </tbody>
+    </table>

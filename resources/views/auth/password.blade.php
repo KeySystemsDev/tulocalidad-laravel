06965f27fd37528e94df4446ce958dc11dd2422c
Carlos Espinoza <carlosarmikhael@gmail.com>
2015-07-02 12:04:27 -0430

    cambio en login

--- resources/views/auth/password.blade.php
+++ resources/views/auth/password.blade.php
@@ -46,7 +46,7 @@
 						<div class="form-group">
 							<div class="col-md-6 col-md-offset-4">
 								<button type="submit" class="btn btn-danger">
-									Enviar Password
+									Enviar Contraseña
 								</button>
 							</div>
 						</div>

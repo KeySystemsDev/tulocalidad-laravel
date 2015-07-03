0b1e1726fda717e35a1098bfa326dd37b588db54
Carlos Espinoza <carlosarmikhael@gmail.com>
2015-07-02 22:54:30 -0430

    cambio en la vista remndados style|

---
+++ resources/views/layouts/nav-top-cliente.blade.php
@@ -0,0 +1,29 @@
+<div class="header-top">
+    <!-- start:navbar -->
+    <nav class="navbar navbar-inverse navbar-static-top" role="navigation" style="margin-bottom: 0">
+        <div class="container">
+            <!-- start:navbar-header -->
+            <div class="navbar-header">
+                <a class="navbar-brand" href="{{ url ('/') }}"><i class="fa fa-map-marker" data-original-title="" title=""></i> <strong>Tu</strong>Localidad<strong>.</strong></a>
+            </div>
+            <ul class="nav navbar-nav navbar-right top-menu">
+                <!-- user login dropdown start-->
+                <li class="dropdown">
+                    <a data-toggle="dropdown" class="dropdown-toggle" href="#">
+                        <img alt="" src="{{ asset('/img/avatar1_small.jpg') }}">
+                        <span class="username">Jhon Doe</span>
+                        <b class="caret"></b>
+                    </a>
+                    <ul class="dropdown-menu">
+                        <div class="log-arrow-up"></div>
+                        <li><a href="#"><i class=" fa fa-suitcase" data-original-title="" title=""></i>Profile</a></li>
+                        <li><a href="#"><i class="fa fa-cog" data-original-title="" title=""></i> Settings</a></li>
+                        <li><a href="#"><i class="fa fa-bell-o" data-original-title="" title=""></i> Notification</a></li>
+                        <li><a href="{{ url ('auth/cerrar') }}"><i class="fa fa-key" data-original-title="" title=""></i> Cerrar sesión</a></li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
+    </nav>
+    <!-- end:navbar -->
+</div>
--- resources/views/servicio/recomendados.blade.php
+++ resources/views/servicio/recomendados.blade.php
@@ -1,18 +1,58 @@
-<h1>Recomendados o Publicidad</h1>
+@extends('base')
 
-<ul>
-	<li><a href="{{ url ('servicios/todo')}}">Estados</a></li>
-</ul>
+@section('content')
+	
+	@include('layouts/nav-top-cliente')
 
-<table border="1">
-	<tr>
-		<td>Imagen</td>
-		<td>Id Publicidad</td>
-	</tr>
-@foreach ($consulta as $key)
-	<tr>
-		<td>{{$key->url_imagen_publicidad}}</td>
-		<td>{{$key->id_publicidad}}</td>
-	</tr>
-@endforeach
-</table>
+	@include('layouts/nav-cliente')
+
+	<div class="container">
+		<div id="main">
+			<div class="row">
+				 <div class="col-lg-12">
+	                   	<section class="panel">                         
+	                        <div class="panel-body">
+
+	                        	<div class="row">
+				                    <div class="col-lg-12">
+				                        <h2 class="page-header">
+				                            Recomendados o Publicidad
+				                        </h2>
+				                    </div>
+				                </div>
+
+				                <div class="row" id="real-estates-columns">
+				                    @foreach ($consulta as $key)
+				                    <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">			                        
+				                        <div class="panel">
+				                            <div class="panel-body">
+				                                <a href="#"><img src="{{$key->url_imagen_publicidad}}" class="img-responsive"></a>
+				                                <div class="title-realestates-columns">
+				                                    <h4><a href="#"><strong>Omah Gedong Apik BGT</strong></a></h4>
+				                                    <hr>
+				                                    <p>Nah nek iki lagi larang soale apik-apik barange tur aku ra iso tuku wong karang jeh ngumpulke gawe nikah kie.</p>
+				                                </div>
+				                            </div>
+				                        </div>                    	
+				                    </div>
+				                    @endforeach
+				                    <!-- start:pagination -->
+				                    <!--<div class="col-lg-12">
+				                        <ul class="pagination pagination-primary pagination-separated">
+				                            <li><a href="#">«</a></li>
+				                            <li><a href="#">1</a></li>
+				                            <li><a href="#">2</a></li>
+				                            <li><a href="#">3</a></li>
+				                            <li><a href="#">»</a></li>
+				                        </ul>
+				                    </div>-->
+				                    <!-- end:pagination -->
+				                </div>
+							</div>
+						</section>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+@endsection

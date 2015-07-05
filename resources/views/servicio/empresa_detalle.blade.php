6ab748098274854d0ebf1102b44ee51dcbdef5ab
Carlos Espinoza <carlosarmikhael@gmail.com>
2015-07-04 21:05:24 -0430

    empresas

--- resources/views/servicio/empresa_detalle.blade.php
+++ resources/views/servicio/empresa_detalle.blade.php
@@ -1,1 +1,11 @@
+@extends('base')
+
+@section('content')
+	
+	@include('layouts/nav-top-cliente')
+
+	@include('layouts/nav-cliente')
+
 <h1>Detalle de la empresa -  {{ $id_empresa }}</h1>
+
+@endsection
--- resources/views/servicio/empresas.blade.php
+++ resources/views/servicio/empresas.blade.php
@@ -6,55 +6,208 @@
 
 	@include('layouts/nav-cliente')
 	
-	<div class="container">
-		<div id="main">
-			<div class="row">
-				 <div class="col-lg-12">
-	                   	<section class="panel">                         
-	                        <div class="panel-body">
+<div class="container">
+    <div id="main">
 
-	                        	<div class="row">
-				                    <div class="col-lg-12">
-				                        <h2 class="page-header">
-				                           <i class="fa fa-rocket"></i> Empresas {{ $id_estado }} {{ $id_categoria }} 
-				                        </h2>
-				                    </div>
-				                </div>
+        <div class="row">
+            <div class="col-lg-12">
+                <h2 class="page-header">
+                    <i class="fa fa-rocket"></i> Empresas {{ $id_estado }} {{ $id_categoria }} 
+                </h2>
+            </div>
+        </div>
 
-				                <div class="row">
-				                    <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
-				                        <div class="panel">
-				                            <div class="panel-body">
-				                                <ul class="list-group">
-				                                    
-				                                    <a class="list-group-item" href="{{ url ('servicios/empresa/'.$id_estado.'/'.$id_categoria.'/key systems')}}">
-				                                        <span class="badge badge-danger">14</span>
-				                                        Key systems                 
-				                                    </a>
-				                                   
-				                                    <a class="list-group-item" href="{{ url ('servicios/empresa/'.$id_estado.'/'.$id_categoria.'/banesco')}}">
-				                                        <span class="badge badge-primary">1</span>
-				                                        Banesco
-				                                    </a>
-				                                    <a class="list-group-item" href="{{ url ('servicios/empresa/'.$id_estado.'/'.$id_categoria.'/adidas')}}">
-				                                        <span class="badge badge-info">24</span>
-				                                        Adidas
-				                                    </a>
-				                                    <a class="list-group-item" href="{{ url ('servicios/empresa/'.$id_estado.'/'.$id_categoria.'/puma')}}">
-				                                        <span class="badge badge-warning">44</span>
-				                                        Puma
-				                                    </a>
-				                                </ul>
-				                            </div>
-				                        </div>
-				                    </div>  
-				                </div>
-							</div>
-						</section>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
+        <!-- start:store list -->
+        <div class="row" id="store-list">
+            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+                <div class="panel">
+                    <div class="panel-body">
+                        <div class="row">
+                            <div class="col-sm-5">
+                                <a href="{{ url ('servicios/empresa/'.$id_estado.'/'.$id_categoria.'/key systems')}}"><img src="img/content/thumbnail1.jpg" class="img-responsive"></a>
+                            </div>
+                            <div class="col-sm-7">
+                                <h4 class="title-store">
+                                    <strong><a href="{{ url ('servicios/empresa/'.$id_estado.'/'.$id_categoria.'/key systems')}}">Key Systems</a></strong>
+                                </h4>
+                                <hr>
+                                <p>Iki kie mung omah lodong dadiine rodo murah tur yo ra awet wong karang mung murah, nek pingin awet yo tuku omah-omahan wae sing ra iso rusak.</p>
+                                <p>
+                                    <a href="#" class="btn btn-default" disabled="" data-original-title="" title="">$12,990</a>
+                                    <a href="#" class="btn btn-warning pull-right" data-original-title="" title="">Buy Now</a>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="panel">
+                    <div class="panel-body">
+                        <div class="row">
+                            <div class="col-sm-5">
+                                <a href="{{ url ('servicios/empresa/'.$id_estado.'/'.$id_categoria.'/banesco')}}"><img src="img/content/thumbnail10.jpg" class="img-responsive"></a>
+                            </div>
+                            <div class="col-sm-7">
+                                <h4 class="title-store">
+                                    <strong><a href="{{ url ('servicios/empresa/'.$id_estado.'/'.$id_categoria.'/banesco')}}">Banesco</a></strong> 
+                                </h4>
+                                <hr>
+                                <p>Iki kie mung omah lodong dadiine rodo murah tur yo ra awet wong karang mung murah, nek pingin awet yo tuku omah-omahan wae sing ra iso rusak.</p>
+                                <p>
+                                    <a href="#" class="btn btn-default" disabled="" data-original-title="" title="">$12,990</a>
+                                    <a href="#" class="btn btn-warning pull-right" data-original-title="" title="">Buy Now</a>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="panel">
+                    <div class="panel-body">
+                        <div class="row">
+                            <div class="col-sm-5">
+                                <a href="{{ url ('servicios/empresa/'.$id_estado.'/'.$id_categoria.'/adidas')}}"><img src="img/content/thumbnail2.jpg" class="img-responsive"></a>
+                            </div>
+                            <div class="col-sm-7">
+                                <h4 class="title-store">
+                                    <strong><a href="{{ url ('servicios/empresa/'.$id_estado.'/'.$id_categoria.'/adidas')}}">Adidas</a></strong>
+                                </h4>
+                                <hr>
+                                <p>Iki kie mung omah lodong dadiine rodo murah tur yo ra awet wong karang mung murah, nek pingin awet yo tuku omah-omahan wae sing ra iso rusak.</p>
+                                <p>
+                                    <a href="#" class="btn btn-default" disabled="" data-original-title="" title="">$12,990</a>
+                                    <a href="#" class="btn btn-warning pull-right" data-original-title="" title="">Buy Now</a>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="panel">
+                    <div class="panel-body">
+                        <div class="row">
+                            <div class="col-sm-5">
+                                <a href="{{ url ('servicios/empresa/'.$id_estado.'/'.$id_categoria.'/puma')}}"><img src="img/content/thumbnail5.jpg" class="img-responsive"></a>
+                            </div>
+                            <div class="col-sm-7">
+                                <h4 class="title-store">
+                                    <strong><a href="{{ url ('servicios/empresa/'.$id_estado.'/'.$id_categoria.'/puma')}}">Puma</a></strong>
+                                </h4>
+                                <hr>
+                                <p>Iki kie mung omah lodong dadiine rodo murah tur yo ra awet wong karang mung murah, nek pingin awet yo tuku omah-omahan wae sing ra iso rusak.</p>
+                                <p>
+                                    <a href="#" class="btn btn-default" disabled="" data-original-title="" title="">$12,990</a>
+                                    <a href="#" class="btn btn-warning pull-right" data-original-title="" title="">Buy Now</a>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+                <div class="panel">
+                    <div class="panel-body">
+                        <div class="row">
+                            <div class="col-sm-5">
+                                <a href="#"><img src="img/content/thumbnail9.jpg" class="img-responsive"></a>
+                            </div>
+                            <div class="col-sm-7">
+                                <h4 class="title-store">
+                                    <strong><a href="#">Omah Lodong</a></strong>
+                                </h4>
+                                <hr>
+                                <p>Iki kie mung omah lodong dadiine rodo murah tur yo ra awet wong karang mung murah, nek pingin awet yo tuku omah-omahan wae sing ra iso rusak.</p>
+                                <p>
+                                    <a href="#" class="btn btn-default" disabled="" data-original-title="" title="">$12,990</a>
+                                    <a href="#" class="btn btn-warning pull-right" data-original-title="" title="">Buy Now</a>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="panel">
+                    <div class="panel-body">
+                        <div class="row">
+                            <div class="col-sm-5">
+                                <a href="#"><img src="img/content/thumbnail1.jpg" class="img-responsive"></a>
+                            </div>
+                            <div class="col-sm-7">
+                                <h4 class="title-store">
+                                    <strong><a href="#">Omah Lodong</a></strong>
+                                </h4>
+                                <hr>
+                                <p>Iki kie mung omah lodong dadiine rodo murah tur yo ra awet wong karang mung murah, nek pingin awet yo tuku omah-omahan wae sing ra iso rusak.</p>
+                                <p>
+                                    <a href="#" class="btn btn-default" disabled="" data-original-title="" title="">$12,990</a>
+                                    <a href="#" class="btn btn-warning pull-right" data-original-title="" title="">Buy Now</a>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="panel">
+                    <div class="panel-body">
+                        <div class="row">
+                            <div class="col-sm-5">
+                                <a href="#"><img src="img/content/thumbnail10.jpg" class="img-responsive"></a>
+                            </div>
+                            <div class="col-sm-7">
+                                <h4 class="title-store">
+                                    <strong><a href="#">Omah Lodong</a></strong>
+                                </h4>
+                                <hr>
+                                <p>Iki kie mung omah lodong dadiine rodo murah tur yo ra awet wong karang mung murah, nek pingin awet yo tuku omah-omahan wae sing ra iso rusak.</p>
+                                <p>
+                                    <a href="#" class="btn btn-default" disabled="" data-original-title="" title="">$12,990</a>
+                                    <a href="#" class="btn btn-warning pull-right" data-original-title="" title="">Buy Now</a>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="panel">
+                    <div class="panel-body">
+                        <div class="row">
+                            <div class="col-sm-5">
+                                <a href="#"><img src="img/content/thumbnail2.jpg" class="img-responsive"></a>
+                            </div>
+                            <div class="col-sm-7">
+                                <h4 class="title-store">
+                                    <strong><a href="#">Omah Lodong</a></strong>
+                                </h4>
+                                <hr>
+                                <p>Iki kie mung omah lodong dadiine rodo murah tur yo ra awet wong karang mung murah, nek pingin awet yo tuku omah-omahan wae sing ra iso rusak.</p>
+                                <p>
+                                    <a href="#" class="btn btn-default" disabled="" data-original-title="" title="">$12,990</a>
+                                    <a href="#" class="btn btn-warning pull-right" data-original-title="" title="">Buy Now</a>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <!-- start:pagination -->
+            <!--<div class="col-lg-12">
+                <ul class="pagination pagination-warning pagination-separated">
+                    <li><a href="#">«</a></li>
+                    <li><a href="#">1</a></li>
+                    <li><a href="#">2</a></li>
+                    <li class="active"><a href="#">..</a></li>
+                    <li><a href="#">32</a></li>
+                    <li><a href="#">33</a></li>
+                    <li><a href="#">»</a></li>
+                </ul>
+            </div>-->
+            <!-- end:pagination -->
+        </div>
+        <!-- end:store list -->
+
+    </div>
+</div>
 
 @endsection

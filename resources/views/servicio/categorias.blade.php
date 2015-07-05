5daefb8d7995496e5b3ad0b4266798b57af91506
Carlos Espinoza <carlosarmikhael@gmail.com>
2015-07-04 20:46:08 -0430

    categorias empresa

--- resources/views/servicio/categorias.blade.php
+++ resources/views/servicio/categorias.blade.php
@@ -1,8 +1,60 @@
-<h1>Estado ---> {{ $id_estado }} </h1>
+@extends('base')
 
-<ul>
-	<li><a href="{{ url ('servicios/categoria/'.$id_estado.'/herreria')}}">herreria</a></li>
-	<li><a href="{{ url ('servicios/categoria/'.$id_estado.'/gruas')}}">gruas</a></li>
-	<li><a href="{{ url ('servicios/categoria/'.$id_estado.'/bancos')}}">bancos</a></li>
-	<li><a href="{{ url ('servicios/categoria/'.$id_estado.'/farmacias')}}">farmacias</a></li>
-</ul>
+@section('content')
+	
+	@include('layouts/nav-top-cliente')
+
+	@include('layouts/nav-cliente')
+
+<div class="container">
+	<div id="main">
+		<div class="row">
+			 <div class="col-lg-12">
+                   	<section class="panel">                         
+                        <div class="panel-body">
+
+                        	<div class="row">
+			                    <div class="col-lg-12">
+			                        <h2 class="page-header">
+			                           <i class="fa fa-globe"></i> Estado {{ $id_estado }}
+			                        </h2>
+			                    </div>
+			                </div>
+
+			                <div class="row">
+			                    <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
+			                        <div class="panel">
+			                            <div class="panel-body">
+			                                <ul class="list-group">
+			                                    
+			                                    <a class="list-group-item" href="{{ url ('servicios/categoria/'.$id_estado.'/herreria')}}">
+			                                        <span class="badge badge-danger">14</span>
+			                                        Herreria                  
+			                                    </a>
+			                                   
+			                                    <a class="list-group-item" href="{{ url ('servicios/categoria/'.$id_estado.'/gruas')}}">
+			                                        <span class="badge badge-primary">1</span>
+			                                        Gruas
+			                                    </a>
+			                                    <a class="list-group-item" href="{{ url ('servicios/categoria/'.$id_estado.'/gruas')}}">
+			                                        <span class="badge badge-info">24</span>
+			                                        Bancos
+			                                    </a>
+			                                    <a class="list-group-item" href="{{ url ('servicios/categoria/'.$id_estado.'/farmacias')}}">
+			                                        <span class="badge badge-warning">44</span>
+			                                        Farmacias
+			                                    </a>
+			                                </ul>
+			                            </div>
+			                        </div>
+			                    </div>  
+			                </div>
+						</div>
+					</section>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+@endsection
--- resources/views/servicio/empresas.blade.php
+++ resources/views/servicio/empresas.blade.php
@@ -1,8 +1,60 @@
-<h1>Empresas ---> {{ $id_estado }} + {{ $id_categoria }} </h1>
+@extends('base')
 
-<ul>
-	<li><a href="{{ url ('servicios/empresa/'.$id_estado.'/'.$id_categoria.'/key systems')}}">key systems</a></li>
-	<li><a href="{{ url ('servicios/empresa/'.$id_estado.'/'.$id_categoria.'/banesco')}}">banesco</a></li>
-	<li><a href="{{ url ('servicios/empresa/'.$id_estado.'/'.$id_categoria.'/adidas')}}">adidas</a></li>
-	<li><a href="{{ url ('servicios/empresa/'.$id_estado.'/'.$id_categoria.'/puma')}}">puma</a></li>
-</ul>
+@section('content')
+	
+	@include('layouts/nav-top-cliente')
+
+	@include('layouts/nav-cliente')
+	
+	<div class="container">
+		<div id="main">
+			<div class="row">
+				 <div class="col-lg-12">
+	                   	<section class="panel">                         
+	                        <div class="panel-body">
+
+	                        	<div class="row">
+				                    <div class="col-lg-12">
+				                        <h2 class="page-header">
+				                           <i class="fa fa-rocket"></i> Empresas {{ $id_estado }} {{ $id_categoria }} 
+				                        </h2>
+				                    </div>
+				                </div>
+
+				                <div class="row">
+				                    <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
+				                        <div class="panel">
+				                            <div class="panel-body">
+				                                <ul class="list-group">
+				                                    
+				                                    <a class="list-group-item" href="{{ url ('servicios/empresa/'.$id_estado.'/'.$id_categoria.'/key systems')}}">
+				                                        <span class="badge badge-danger">14</span>
+				                                        Key systems                 
+				                                    </a>
+				                                   
+				                                    <a class="list-group-item" href="{{ url ('servicios/empresa/'.$id_estado.'/'.$id_categoria.'/banesco')}}">
+				                                        <span class="badge badge-primary">1</span>
+				                                        Banesco
+				                                    </a>
+				                                    <a class="list-group-item" href="{{ url ('servicios/empresa/'.$id_estado.'/'.$id_categoria.'/adidas')}}">
+				                                        <span class="badge badge-info">24</span>
+				                                        Adidas
+				                                    </a>
+				                                    <a class="list-group-item" href="{{ url ('servicios/empresa/'.$id_estado.'/'.$id_categoria.'/puma')}}">
+				                                        <span class="badge badge-warning">44</span>
+				                                        Puma
+				                                    </a>
+				                                </ul>
+				                            </div>
+				                        </div>
+				                    </div>  
+				                </div>
+							</div>
+						</section>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+@endsection

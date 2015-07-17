6ce5b113de374dae9582496f570f4b9d7218299b
Carlos Espinoza <carlosarmikhael@gmail.com>
2015-07-17 12:08:36 -0430

    cambio en vista nav top login boby

--- resources/views/layouts/nav-top-auth.blade.php
+++ resources/views/layouts/nav-top-auth.blade.php
@@ -4,13 +4,13 @@
         <div class="container">
             <!-- start:navbar-header -->
             <div class="navbar-header">
-                <a class="navbar-brand" href="{{ url ('/') }}"><i class="fa fa-map-marker" data-original-title="" title=""></i> <strong>Tu</strong>Localidad<strong>.</strong></a>
+                <a class="navbar-brand" href="{{ url ('/servicios') }}"><i class="fa fa-map-marker" data-original-title="" title=""></i> <strong>Tu</strong>Localidad<strong>.</strong></a>
             </div>
             <ul class="nav navbar-nav navbar-right nav-top-auth-button-intro">
                 <!-- user login dropdown start-->
                 <a class="auth-intro-button" href="{{ url ('/auth/login') }}">
-                    <i class="fa fa-home"></i>
-                    <span class="username">Entrar</span>
+                    <span class="username">Iniciar sesión</span>
+                    <i class="fa fa-sign-in"></i>
                 </a>
             </ul>
         </div>

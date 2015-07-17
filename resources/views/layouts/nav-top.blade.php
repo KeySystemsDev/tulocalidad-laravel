abb9a43b55bcd47c64e60510464de6929401b6a1
Carlos Espinoza <carlosarmikhael@gmail.com>
2015-07-17 11:53:37 -0430

    cambio en nav top

--- resources/views/layouts/nav-top.blade.php
+++ resources/views/layouts/nav-top.blade.php
@@ -13,6 +13,30 @@
                 </li>-->
                 <!-- user login dropdown start-->
                 <li class="dropdown">
+                    <a class="auth-intro-button" href="{{ url ('/auth/register') }}">
+                        <i class="fa fa-pencil-square-o"></i>
+                        <span class="username">Registrarte</span>
+                    </a>
+                </li>
+            </ul>
+            <ul class="nav navbar-nav navbar-right top-menu">
+                <!--<li>
+                    <input type="text" class="form-control input-sm search" placeholder="Search">
+                </li>-->
+                <!-- user login dropdown start-->
+                <li class="dropdown">
+                    <a class="auth-intro-button" href="{{ url ('/auth/login') }}">
+                        <i class="fa fa-sign-in"></i>
+                        <span class="username">Iniciar sesión</span>
+                    </a>
+                </li>
+            </ul>
+            <ul class="nav navbar-nav navbar-right top-menu">
+                <!--<li>
+                    <input type="text" class="form-control input-sm search" placeholder="Search">
+                </li>-->
+                <!-- user login dropdown start-->
+                <li class="dropdown">
                     <a data-toggle="dropdown" class="dropdown-toggle" href="#">
                         <img alt="" src="{{ asset('/img/avatar1_small.jpg') }}">
                         <span class="username">{{\Session::get('usuario')}}</span>
@@ -23,7 +47,7 @@
                         <!--<li><a href="#"><i class=" fa fa-suitcase" data-original-title="" title=""></i>Profile</a></li>
                         <li><a href="#"><i class="fa fa-cog" data-original-title="" title=""></i> Settings</a></li>
                         <li><a href="#"><i class="fa fa-bell-o" data-original-title="" title=""></i> Notification</a></li>-->
-                        <li><a href="{{ url ('auth/cerrar') }}"><i class="fa fa-key" data-original-title="" title=""></i> Cerrar sesión</a></li>
+                        <li><a href="{{ url ('auth/cerrar') }}"><i class="fa fa-sign-in" data-original-title="" title=""></i> Cerrar sesión</a></li>
                     </ul>
                 </li>
             </ul>

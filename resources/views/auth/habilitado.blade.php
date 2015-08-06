fadc7a2850b3d2b2ed49b0ffc991c5970d0efa6e
Carlos Espinoza <carlosarmikhael@gmail.com>
2015-08-06 14:40:20 -0430

    vista de confirmacion usuario

--- resources/views/auth/habilitado.blade.php
+++ resources/views/auth/habilitado.blade.php
@@ -1,13 +1,44 @@
 @extends('base')
 
-@if ($codigo == 1)
-	<h1>Usuario Habilitado Satisfactoriamente</h1>
-@endif
+<ul class="cb-slideshow ul-login">
+    <li class="li-login"><span></span></li>
+    <li class="li-login"><span></span></li>
+    <li class="li-login"><span></span></li>
+    <li class="li-login"><span></span></li>
+    <li class="li-login"><span></span></li>
+    <li class="li-login"><span></span></li>
+</ul>
 
-@if ($codigo == 2)
-	<h1>Codigo Usado. Usuario ya fue habilitado</h1>
-@endif
+@section('content')
+		
+	@include('layouts/nav-top-auth')
+	
+	<div class="container-fluid">
 
-@if ($codigo == -1)
-	<h1>Codigo invalido</h1>
-@endif
+		<div class="row login-tulocalidad">
+			<div class="col-md-6 col-md-offset-3">
+				<div class="panel panel-default border-tulocalidad">
+					<center><img class="img-registrar-logo" src="{{ asset('/img/tulocalidad-blanco.png') }}"></center>
+					<div class="panel-body">
+
+						<center>
+							@if ($codigo == 1)
+								<label class="control-label">Usuario Habilitado Satisfactoriamente</label>
+							@endif
+
+							@if ($codigo == 2)
+								<label class="control-label">Codigo Usado. Usuario ya fue habilitado</label>
+							@endif
+
+							@if ($codigo == -1)
+								<label class="control-label">Codigo invalido</label>
+							@endif
+						</center>
+
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+@endsection

5404a41523b6db370f4c0cad8b7ca165c6ce654e
Carlos Espinoza <carlosarmikhael@gmail.com>
2015-10-22 16:01:46 -0430

    thema nuevo para tulocalidad

---
+++ resources/views/base-admin.blade.php
@@ -0,0 +1,151 @@
+<!DOCTYPE html>
+<html lang="en" ng-app="wanaitravel">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content='{{ csrf_token() }}'>
+
+    <title>.:Tu Localidad | Venezuela:.</title>
+
+    <link href="{{ asset('/css/app.css') }}" rel="stylesheet">
+
+    <!-- angular modules style -->
+    <link href="{{ asset('/bower_components/ngImgCrop/compile/minified/ng-img-crop.css') }}" rel="stylesheet">
+
+    <!-- bootstrap modules style -->
+    <link href="{{ asset('/bower_components/bootstrap/dist/css/bootstrap.min.css') }}" rel="stylesheet">
+	<!-- bootstrap jasny-->
+	<link href="{{ asset('/bower_components/jasny-bootstrap/dist/css/jasny-bootstrap.min.css') }}" rel="stylesheet" type="text/css">
+
+	<!-- Fonts -->
+	<link href='//fonts.googleapis.com/css?family=Roboto:400,300' rel='stylesheet' type='text/css'>
+	<link rel="shortcut icon" href="favicon.ico" />
+	<link href="{{ asset('/bower_components/font-awesome/css/font-awesome.css') }}" rel="stylesheet" type="text/css">
+	
+	<!-- Thema CSS -->
+	<link href="{{ asset('/thema/assets/plugins/jquery-ui/themes/base/minified/jquery-ui.min.css') }}" rel="stylesheet" />
+	<link href="{{ asset('/thema/assets/css/animate.min.css') }}" rel="stylesheet" type="text/css">
+	<link href="{{ asset('/thema/assets/css/style.min.css') }}" rel="stylesheet" type="text/css">
+	<link href="{{ asset('/thema/assets/css/style-responsive.min.css') }}" rel="stylesheet" type="text/css">
+	<link href="{{ asset('/thema/assets/css/theme/default.css') }}" rel="stylesheet" type="text/css">
+	<link href="{{ asset('/thema/assets/plugins/bootstrap-calendar/css/bootstrap_calendar.css') }}" rel="stylesheet" />
+    <link href="{{ asset('/thema/assets/plugins/gritter/css/jquery.gritter.css') }}" rel="stylesheet" />
+    <link href="{{ asset('/thema/assets/plugins/morris/morris.css') }}" rel="stylesheet" />
+    <!-- ================== BEGIN PAGE LEVEL STYLE ================== -->
+	<link href="{{ asset('/thema/assets/plugins/bootstrap-wizard/css/bwizard.min.css') }}" rel="stylesheet" />
+	<!-- ================== END PAGE LEVEL STYLE ================== -->
+	<!-- ================== BEGIN PAGE LEVEL STYLE ================== -->
+	<link href="{{ asset('/thema/assets/plugins/bootstrap-datepicker/css/datepicker.css') }}" rel="stylesheet" />
+	<link href="{{ asset('/thema/assets/plugins/bootstrap-datepicker/css/datepicker3.css') }}" rel="stylesheet" />
+	<link href="{{ asset('/thema/assets/plugins/ionRangeSlider/css/ion.rangeSlider.css') }}" rel="stylesheet" />
+	<link href="{{ asset('/thema/assets/plugins/ionRangeSlider/css/ion.rangeSlider.skinNice.css') }}" rel="stylesheet" />
+	<link href="{{ asset('/thema/assets/plugins/bootstrap-colorpicker/css/bootstrap-colorpicker.min.css') }}" rel="stylesheet" />
+	<link href="{{ asset('/thema/assets/plugins/bootstrap-timepicker/css/bootstrap-timepicker.min.css') }}" rel="stylesheet" />
+	<link href="{{ asset('/thema/assets/plugins/password-indicator/css/password-indicator.css') }}" rel="stylesheet" />
+	<link href="{{ asset('/thema/assets/plugins/bootstrap-combobox/css/bootstrap-combobox.css') }}" rel="stylesheet" />
+	<link href="{{ asset('/thema/assets/plugins/bootstrap-select/bootstrap-select.min.css') }}" rel="stylesheet" />
+	<link href="{{ asset('/thema/assets/plugins/bootstrap-tagsinput/bootstrap-tagsinput.css') }}" rel="stylesheet" />
+	<link href="{{ asset('/thema/assets/plugins/jquery-tag-it/css/jquery.tagit.css') }}" rel="stylesheet" />
+    <link href="{{ asset('/thema/assets/plugins/bootstrap-daterangepicker/daterangepicker-bs3.css') }}" rel="stylesheet" />
+
+	<!-- select2 -->
+	<link href="{{ asset('/bower_components/select2/dist/css/select2.min.css') }}" rel="stylesheet" />
+	<link href="{{ asset('/css/style.css') }}" rel="stylesheet">
+
+  	<!-- ui-select files -->
+	
+	<link rel="stylesheet" href="{{ asset('/bower_components/ui-select/dist/select.css') }}">	
+	
+	<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+	<!--[if lt IE 9]>
+		<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+		<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+	<![endif]-->
+	<script src="{{ asset('/thema/assets/plugins/pace/pace.min.js') }}"></script>
+</head>
+<body>
+	
+	<div id="page-loader" class="fade in"><span class="spinner"></span></div>
+	
+	@yield('content')
+
+	<a href="javascript:;" class="btn btn-icon btn-circle btn-success btn-scroll-to-top fade" data-click="scroll-top"><i class="fa fa-angle-up"></i></a>
+	<!-- end scroll to top btn -->
+
+    <!-- jquery modules -->
+    <script src="{{ asset('/bower_components/jquery/dist/jquery.min.js') }}"></script>
+    <script src="{{ asset('/bower_components/jquery-migrate/jquery-migrate.min.js') }}"></script>
+    <script src="{{ asset('/thema/assets/plugins/jquery-ui/ui/minified/jquery-ui.min.js') }}"></script>
+
+
+    <!-- Scripts -->
+    <script src="{{ asset('/bower_components/angular/angular.js') }}"></script>
+
+    <!-- your app's js -->
+    <script src="{{ asset('/js/app.js') }}"></script>
+    <script src="{{ asset('/js/controller.js') }}"></script>
+    <script src="{{ asset('/js/service.js') }}"></script>
+    <script src="{{ asset('/js/directives.js') }}"></script>
+
+    <!-- angular modules -->
+    <script src="{{ asset('/bower_components/ngImgCrop/compile/minified/ng-img-crop.js') }}"></script>
+    <script src="{{ asset('/bower_components/angular-base64/angular-base64.min.js') }}"></script>
+    <script src="{{ asset('/bower_components/angular-resource/angular-resource.min.js') }}"></script>
+    <script src="{{ asset('/bower_components/ui-select/dist/select.js') }}"></script>
+    <script src="{{ asset('/bower_components/angular-sanitize/angular-sanitize.js') }}"></script>
+
+
+    <!-- bootstrap modules -->
+    <script src="{{ asset('/bower_components/bootstrap/dist/js/bootstrap.min.js') }}"></script>
+	<!-- bootstrap jasny-->
+    <script src="{{ asset('/bower_components/jasny-bootstrap/dist/js/jasny-bootstrap.min.js') }}"></script>
+	
+	<!-- Thema JS -->
+	<script src="{{ asset('/thema/assets/plugins/slimscroll/jquery.slimscroll.min.js') }}"></script>
+	<script src="{{ asset('/thema/assets/plugins/jquery-cookie/jquery.cookie.js') }}"></script>
+	<script src="{{ asset('/thema/assets/plugins/morris/raphael.min.js') }}"></script>
+    <script src="{{ asset('/thema/assets/plugins/morris/morris.js') }}"></script>
+    <script src="{{ asset('/thema/assets/plugins/jquery-jvectormap/jquery-jvectormap-1.2.2.min.js') }}"></script>
+    <script src="{{ asset('/thema/assets/plugins/jquery-jvectormap/jquery-jvectormap-world-merc-en.js') }}"></script>
+    <script src="{{ asset('/thema/assets/plugins/bootstrap-calendar/js/bootstrap_calendar.min.js') }}"></script>
+	<script src="{{ asset('/thema/assets/plugins/gritter/js/jquery.gritter.js') }}"></script>
+	<script src="{{ asset('/thema/assets/js/dashboard-v2.min.js') }}"></script>
+	<!-- ================== BEGIN PAGE LEVEL JS ================== -->
+	<script src="{{ asset('/thema/assets/plugins/bootstrap-wizard/js/bwizard.js') }}"></script>
+	<script src="{{ asset('/thema/assets/js/form-wizards.demo.min.js') }}"></script>
+	<!-- select -->
+	<script src="{{ asset('/bower_components/jquery-placeholder/jquery.placeholder.min.js') }}"></script>
+	<script src="{{ asset('/bower_components/select2/dist/js/select2.min.js') }}"></script>
+	<!-- ================== BEGIN PAGE LEVEL JS ================== -->
+	<script src="{{ asset('/thema/assets/js/login-v2.demo.min.js') }}"></script>
+	<!-- ================== BEGIN PAGE LEVEL JS ================== -->
+	<!-- ================== BEGIN PAGE LEVEL JS ================== -->
+	<script src="{{ asset('/thema/assets/plugins/bootstrap-datepicker/js/bootstrap-datepicker.js') }}"></script>
+	<script src="{{ asset('/thema/assets/plugins/ionRangeSlider/js/ion-rangeSlider/ion.rangeSlider.min.js') }}"></script>
+	<script src="{{ asset('/thema/assets/plugins/bootstrap-colorpicker/js/bootstrap-colorpicker.min.js') }}"></script>
+	<script src="{{ asset('/thema/assets/plugins/masked-input/masked-input.min.js') }}"></script>
+	<script src="{{ asset('/thema/assets/plugins/bootstrap-timepicker/js/bootstrap-timepicker.min.js') }}"></script>
+	<script src="{{ asset('/thema/assets/plugins/password-indicator/js/password-indicator.js') }}"></script>
+	<script src="{{ asset('/thema/assets/plugins/bootstrap-combobox/js/bootstrap-combobox.js') }}"></script>
+	<script src="{{ asset('/thema/assets/plugins/bootstrap-select/bootstrap-select.min.js') }}"></script>
+	<script src="{{ asset('/thema/assets/plugins/bootstrap-tagsinput/bootstrap-tagsinput.min.js') }}"></script>
+	<script src="{{ asset('/thema/assets/plugins/bootstrap-tagsinput/bootstrap-tagsinput-typeahead.js') }}"></script>
+	<script src="{{ asset('/thema/assets/plugins/jquery-tag-it/js/tag-it.min.js') }}"></script>
+    <script src="{{ asset('/thema/assets/plugins/bootstrap-daterangepicker/moment.js') }}"></script>
+    <script src="{{ asset('/thema/assets/plugins/bootstrap-daterangepicker/daterangepicker.js') }}"></script>
+	<script src="{{ asset('/thema/assets/js/form-plugins.demo.min.js') }}"></script>
+	<script src="{{ asset('/thema/assets/js/apps.min.js') }}"></script>
+	
+	<!-- ===================== ANGULAR CONTROLLERS ==============================-->
+	@yield('controller')
+
+	<script>
+		$(document).ready(function() {
+			App.init();
+		});
+	</script>
+	
+</body>
+</html>
---
+++ resources/views/base-cliente.blade.php
@@ -0,0 +1,105 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content='{{ csrf_token() }}'>
+
+    <title>.:Tu Localidad | Venezuela:.</title>
+	
+	<link href="{{ asset('/css/app.css') }}" rel="stylesheet">
+    
+
+    <!-- angular modules style -->
+    <link href="{{ asset('/bower_components/ngImgCrop/compile/minified/ng-img-crop.css') }}" rel="stylesheet">
+
+	<!-- Fonts -->
+	<link href='//fonts.googleapis.com/css?family=Roboto:400,300' rel='stylesheet' type='text/css'>
+	<link rel="shortcut icon" href="favicon.ico" />
+
+	<link href="{{ asset('/bower_components/font-awesome/css/font-awesome.css') }}" rel="stylesheet" type="text/css">
+	
+	<link href="{{ asset('/thema/assets-frontend/css/style-theme.css') }}" rel="stylesheet" type="text/css">
+	<link href="{{ asset('/thema/assets-frontend/css/style-responsive-theme.css') }}" rel="stylesheet" type="text/css">
+	<!-- Thema CSS -->
+	<link href="{{ asset('/thema/assets/plugins/jquery-ui/themes/base/minified/jquery-ui.min.css') }}" rel="stylesheet" />
+	<link href="{{ asset('/thema/assets-frontend/css/animate.min.css') }}" rel="stylesheet" />
+	<link href="{{ asset('/thema/assets-frontend/css/style.min.css') }}" rel="stylesheet" />
+	<link href="{{ asset('/thema/assets-frontend/css/style-responsive.min.css') }}" rel="stylesheet" />
+	<link href="{{ asset('/thema/assets-frontend/css/theme/default.css') }}" id="theme" rel="stylesheet" />
+	<!-- ================== END BASE CSS STYLE ================== -->
+	
+	<!-- ================== BEGIN PAGE LEVEL STYLE ================== -->
+  	<link href="{{ asset('/thema/assets/plugins/isotope/isotope.css') }}" rel="stylesheet" />
+  	<link href="{{ asset('/thema/assets/plugins/lightbox/css/lightbox.css') }}" rel="stylesheet" />
+	<!-- ================== END PAGE LEVEL STYLE ================== -->
+	
+	<!-- ================== gallery ================== -->
+	<link rel="stylesheet" type="text/css" href="{{ asset('/scatteredpolaroidsgallery/css/component.css') }}" />
+	<script src="{{ asset('/scatteredpolaroidsgallery/js/modernizr.min.js') }}"></script>
+
+	<!-- ================== BEGIN BASE JS ================== -->
+	<script src="{{ asset('/thema/assets-frontend/plugins/pace/pace.min.js') }}"></script>
+	<!-- ================== END BASE JS ================== -->
+
+	<link href="{{ asset('/css/style.css') }}" rel="stylesheet">
+
+</head>
+<body data-spy="scroll" data-target="#header-navbar" data-offset="51">
+	
+	@yield('content')
+
+    <!-- jquery modules -->
+    <script src="{{ asset('/bower_components/jquery/dist/jquery.min.js') }}"></script>
+    <script src="{{ asset('/bower_components/jquery-migrate/jquery-migrate.min.js') }}"></script>
+    <script src="{{ asset('/thema/assets/plugins/jquery-ui/ui/minified/jquery-ui.min.js') }}"></script>
+
+	<!-- bootstrap modules -->
+    <script src="{{ asset('/bower_components/bootstrap/dist/js/bootstrap.min.js') }}"></script>
+
+    <!-- Scripts -->
+    <script src="{{ asset('/bower_components/angular/angular.js') }}"></script>
+
+    <!-- your app's js -->
+    <script src="{{ asset('/js/app.js') }}"></script>
+    <script src="{{ asset('/js/controller.js') }}"></script>
+    <script src="{{ asset('/js/directives.js') }}"></script>
+    <script src="{{ asset('/js/service.js') }}"></script>
+
+    <!-- angular modules -->
+    <script src="{{ asset('/bower_components/ngImgCrop/compile/minified/ng-img-crop.js') }}"></script>
+    <script src="{{ asset('/bower_components/angular-base64/angular-base64.min.js') }}"></script>
+    <script src="{{ asset('/bower_components/ui-select/dist/select.js') }}"></script>
+    <script src="{{ asset('/bower_components/angular-sanitize/angular-sanitize.js') }}"></script>
+    <script src="{{ asset('/bower_components/angular-resource/angular-resource.min.js') }}"></script>
+
+	<!-- bootstrap jasny-->
+    <script src="{{ asset('/bower_components/jasny-bootstrap/dist/js/jasny-bootstrap.min.js') }}"></script>
+	
+	<!-- Thema JS -->
+	<script src="{{ asset('/thema/assets-frontend/plugins/jquery-cookie/jquery.cookie.js') }}"></script>
+	<script src="{{ asset('/thema/assets-frontend/plugins/scrollMonitor/scrollMonitor.js') }}"></script>
+	<!-- ================== BEGIN PAGE LEVEL JS ================== -->
+	<script src="{{ asset('/thema/assets/plugins/isotope/jquery.isotope.min.js') }}"></script>
+  	<script src="{{ asset('/thema/assets/plugins/lightbox/js/lightbox-2.6.min.js') }}"></script>
+	<script src="{{ asset('/thema/assets/js/gallery.demo.min.js') }}"></script>
+	<!-- ================== END PAGE LEVEL JS ================== -->
+	
+	<script src="{{ asset('/thema/assets-frontend/js/apps.min.js') }}"></script>
+	
+	<!-- ================== Gallery ================== -->
+	<script src="{{ asset('/scatteredpolaroidsgallery/js/classie.js') }}"></script>
+	<script src="{{ asset('/scatteredpolaroidsgallery/js/photostack.js') }}"></script>
+
+	<!-- ===================== ANGULAR CONTROLLERS ==============================-->
+	@yield('controller')
+	
+	<script>
+		$(document).ready(function() {
+			App.init();
+		});
+	</script>
+	
+</body>
+</html>

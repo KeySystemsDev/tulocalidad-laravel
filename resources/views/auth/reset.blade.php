ca0b321a467353ae64e614be8df505f018cad133
Carlos Espinoza <carlosarmikhael@gmail.com>
2015-10-27 22:43:09 -0430

    reset contraseña

--- resources/views/auth/reset.blade.php
+++ resources/views/auth/reset.blade.php
@@ -1,4 +1,4 @@
-@extends('base')
+@extends('base-cliente')
 
 @section('content')
 
@@ -6,109 +6,116 @@
 	<script src="{{ asset('/js/controllers/auth/reset.js') }}"></script>
 @endsection
 
-	@include('layouts/nav-top')
+<div id="page-container" class="fade page-sidebar-fixed page-header-fixed">
 
-	<div class="container conf" ng-controller="ResetContraseñaController">
-		<div class="row">
-			 <div class="col-lg-12 box-conf">
-				 			
-				<div class="row" id="real-estates-detail">
-                    
-                    <div class="col-lg-3 col-md-3 col-xs-12">
-                        <div class="panel panel-default item-perfil">
-                            <div class="panel-body">
-                                <div class="text-center" id="author">
-                                    <img class="img-perfil img-5" src="{{asset('img/usuario.png')}}">
-                                    <p>{{\Session::get('usuario')}}</p>
-                                </div>
-                            </div>
-                        </div>
+	@include('layouts/navbar-cliente')
 
-                        <div class="row">
-                            <div class="col-lg-12">
-                                <ul class="list-group">
-                                	<a class="list-group-item item-i"> Modificar contraseña <i class="fa fa-chevron-right fa-1x"></i></a>
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
-                    
-                    <div class="col-lg-6 col-md-6 col-xs-12">
-                        <div class="panel item-perfil">
-                            <div class="panel-body">
+	<!-- begin #contact -->
+   	<div class="content bg-silver-lighter" ng-controller="ResetContraseñaController">
+		<!-- begin container -->
+        <div class="container">
+	
+			<div class="row">
+				 <div class="col-lg-12">
+					 			
+					<div class="row" id="real-estates-detail">
+	                    
+	                    <div class="col-lg-3 col-md-3 col-xs-12">
+	                        <div class="panel panel-default item-perfil">
+	                            <div class="panel-body">
+	                                <div class="text-center" id="author">
+	                                    <img class="img-perfil img-5" src="{{asset('img/usuario.png')}}">
+	                                    <br><br>
+	                                    <p>{{\Session::get('usuario')}}</p>
+	                                </div>
+	                            </div>
+	                        </div>
 
-                            	<h4 class="page-header">
-                            		Modificar contraseña
-                        		</h4>
-                        		<p>Cambia o recupera tu contraseña actual.</p>
+	                        <div class="row">
+	                            <div class="col-lg-12">
+	                                <ul class="list-group">
+	                                	<a class="list-group-item item-i"> Modificar contraseña <i class="fa fa-chevron-right fa-1x"></i></a>
+	                                </ul>
+	                            </div>
+	                        </div>
+	                    </div>
+	                    
+	                    <div class="col-lg-6 col-md-6 col-xs-12">
+	                        <div class="panel item-perfil">
+	                            <div class="panel-body">
 
-                        		<br><br>
+	                            	<h4 class="page-header">
+	                            		Modificar contraseña
+	                        		</h4>
+	                        		<p>Cambia o recupera tu contraseña actual.</p>
 
-                        		<form class="form-horizontal" role="form" method="POST" id="formulario" name="formulario" action="{{ url('/password/reset') }}">
+	                        		<br><br>
 
-									<div class="row">
-										<div class="col-lg-3 col-lg-offset-4"><h6>* Datos requeridos</h6></div>
-									</div>
-									<div class="form-group">
-										<label class="col-md-4 control-label">Contraseña actual: *</label>
-										<div class="col-md-6">
-											<input type="password" class="form-control" ng-model="pw_old" name="password_old" required>
+	                        		<form class="form-horizontal" role="form" method="POST" id="formulario" name="formulario" action="{{ url('/password/reset') }}">
+
+										<div class="row">
+											<div class="col-lg-3 col-lg-offset-4"><h6>* Datos requeridos</h6></div>
+										</div>
+										<div class="form-group">
+											<label class="col-md-4 control-label">Contraseña actual: *</label>
+											<div class="col-md-6">
+												<input type="password" class="form-control" ng-model="pw_old" name="password_old" required>
+											</div>
+
+										</div>
+										
+										<div class="form-group">
+											<label class="col-md-4 control-label"></label>
+											<div class="col-md-6">
+												<a href="{{ url('/password/email') }}">¿Olvidaste tu contraseña?</a>
+											</div>
 										</div>
 
-									</div>
-									
-									<div class="form-group">
-										<label class="col-md-4 control-label"></label>
-										<div class="col-md-6">
-											<a href="{{ url('/password/email') }}">¿Olvidaste tu contraseña?</a>
+										<div class="form-group">
+											<label class="col-md-4 control-label">Nueva contraseña: *</label>
+											<div class="col-md-6">
+												<input type="password" class="form-control" name="password" ng-model="pw" ng-blur="validar_pass1()"
+														name="pw" id="pw" ng-class="{'error':error_pass1 && submit_pass1}" required >
+												<div class="col-lg-10" ng-show="error_pass1 && submit_pass1" ng-cloak>
+						        					<p class="help-block" ng-cloak>[[msj_error_pass1]]</p>
+						      					</div>	
+											</div>
 										</div>
-									</div>
 
-									<div class="form-group">
-										<label class="col-md-4 control-label">Nueva contraseña: *</label>
-										<div class="col-md-6">
-											<input type="password" class="form-control" name="password" ng-model="pw" ng-blur="validar_pass1()"
-													name="pw" id="pw" ng-class="{'error':error_pass1 && submit_pass1}" required >
-											<div class="col-lg-10" ng-show="error_pass1 && submit_pass1" ng-cloak>
-					        					<p class="help-block" ng-cloak>[[msj_error_pass1]]</p>
-					      					</div>	
+										<div class="form-group">
+											<label class="col-md-4 control-label">Confirmar contraseña: *</label>
+											<div class="col-md-6">
+												<input type="password" class="form-control" name="password2" ng-model="pw2" ng-blur="validar_pass2()"
+														 name="pw2" id="pw2" ng-class="{'error':error_pass2 && submit_pass2}" required>
+												<ul id="strength" check-strength="pw"></ul>
+												<div class="col-lg-10" ng-show="error_pass2 && submit_pass2" ng-cloak>
+						        					<p class="help-block" ng-cloak>[[msj_error_pass2]]</p>
+						      					</div>	
+											</div>						
 										</div>
-									</div>
-
-									<div class="form-group">
-										<label class="col-md-4 control-label">Confirmar contraseña: *</label>
-										<div class="col-md-6">
-											<input type="password" class="form-control" name="password2" ng-model="pw2" ng-blur="validar_pass2()"
-													 name="pw2" id="pw2" ng-class="{'error':error_pass2 && submit_pass2}" required>
-											<ul id="strength" check-strength="pw"></ul>
-											<div class="col-lg-10" ng-show="error_pass2 && submit_pass2" ng-cloak>
-					        					<p class="help-block" ng-cloak>[[msj_error_pass2]]</p>
-					      					</div>	
-										</div>						
-									</div>
-									
-									<br>
-									<div class="form-group">
-										<div class="col-md-3 col-md-offset-4">
-											<button type="button" ng-click="checkMe()" ng-disabled="formulario.$invalid || error_pass2 || error_pass1" class="btn btn-info">
-												Guardar
-											</button>
+										
+										<br>
+										<div class="form-group">
+											<div class="col-md-3 col-md-offset-4">
+												<button type="button" ng-click="checkMe()" ng-disabled="formulario.$invalid || error_pass2 || error_pass1" class="btn btn-info">
+													Guardar
+												</button>
+											</div>
 										</div>
-									</div>
-								</form>
+									</form>
 
-                                
-                            </div>
-                        </div>
-                    </div>
-                </div>
+	                                
+	                            </div>
+	                        </div>
+	                    </div>
+	                </div>
 
-		
+				</div>
+				@include('modals/validacion_modal')
 			</div>
 		</div>
-		@include('modals/validacion_modal')
 	</div>
-
 	@include('layouts/footer')
+</div>
 
 @endsection

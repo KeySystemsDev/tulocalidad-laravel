45216855a53a25968b7f8e25a1acfe32112b3805
Carlos Espinoza <carlosarmikhael@gmail.com>
2015-12-09 18:46:14 -0430

    registro

--- resources/views/autenticacion/registro.blade.php
+++ resources/views/autenticacion/registro.blade.php
@@ -1,53 +1,89 @@
-@extends('base-admin')
+@extends('base-cliente')
 
 @section('content')
-<div class="container-fluid">
-	<div class="row">
-		<div class="col-md-8 col-md-offset-2">
-			<div class="panel panel-default">
-				<div class="panel-heading">Register</div>
-				<div class="panel-body">
-					<br>
-					<br>
-					<br>
-					<br>
-					@include('alerts.mensaje_success')
-					@include('alerts.mensaje_error')
 
-					<form class="form-horizontal" role="form" method="POST" action="{{ url('/registrar') }}">
-						<input type="hidden" name="_token" value="{{ csrf_token() }}">
+<div id="page-container" class="fade">
 
-						<div class="form-group">
-							<label class="col-md-4 control-label">E-Mail Address</label>
-							<div class="col-md-6">
-								<input type="email" class="form-control" name="correo_usuario" value="{{ old('email') }}">
-							</div>
-						</div>
+	<div class="login login-with-news-feed">
+        <!-- begin news-feed -->
+        <div class="news-feed">
+            <div class="news-image">
+            	<ul class="cb-slideshow ul-login">
+			        <li class="li-login"><span></span></li>
+			        <li class="li-login"><span></span></li>
+			        <li class="li-login"><span></span></li>
+			        <li class="li-login"><span></span></li>
+			        <li class="li-login"><span></span></li>
+			        <li class="li-login"><span></span></li>
+			    </ul>
+                <!--<img src="{{ asset('/img/bg-7.jpg') }}" data-id="login-cover-image" alt="">-->
+            </div>
+            <div class="news-caption">
+                <h4 class="caption-title"> <img class="login-icono" src="{{ asset('/img/icono.png') }}">  TuLocalidad </h4>
+                <p>
+                    Lo que buscas a la mano.
+                </p>
+            </div>
+        </div>
+        <!-- end news-feed -->
+        <!-- begin right-content -->
+        <div class="right-content">
+            <!-- begin login-header -->
+            <div class="login-header">
+                <div class="brand">
+                    <img class="login-icono" src="{{ asset('/img/icono.png') }}"> TuLocalidad
+                    <small> Un directorio pensado para ti...</small>
+                </div>
+                <div class="icon">
+                    <i class="fa fa-pencil-square-o"></i>
+                </div>
+            </div>
+            <!-- end login-header -->
+            <!-- begin login-content -->
+            <div class="login-content">
+					
+				@include('alerts.mensaje_success')
+				@include('alerts.mensaje_error')
 
-						<div class="form-group">
-							<label class="col-md-4 control-label">Password</label>
-							<div class="col-md-6">
-								<input type="text" class="form-control" name="password">
-							</div>
-						</div>
-						<div class="form-group">
-							<label class="col-md-4 control-label">repeat Password</label>
-							<div class="col-md-6">
-								<input type="text" class="form-control" name="re-password">
-							</div>
-						</div>						
+				<form class="form-horizontal" role="form" method="POST" action="{{ url('/registrar') }}">
+					<input type="hidden" name="_token" value="{{ csrf_token() }}">
 
-						<div class="form-group">
-							<div class="col-md-6 col-md-offset-4">
-								<button type="submit" class="btn btn-primary">
-									Register
-								</button>
-							</div>
-						</div>
-					</form>
-				</div>
+					<div class="form-group m-b-15">
+						<input type="email" class="form-control" name="correo_usuario" value="{{ old('email') }}" placeholder="Correo Electronico">
+					</div>
+
+					<div class="form-group m-b-15">
+						<input type="text" class="form-control" name="password" placeholder="Contraseña">
+					</div>
+					<div class="form-group m-b-15">
+						<input type="text" class="form-control" name="re-password" placeholder="Repetir Contraseña">
+					</div>						
+			
+					<br>
+
+					<div class="login-buttons">
+						<button type="submit" class="btn btn-danger btn-block btn-lg">
+							Registrar
+						</button>
+					</div>
+
+					<hr>
+                    <p class="text-center text-inverse">
+                        © Copyright Key Systems C.A 2015
+                    </p>
+
+                    <p class="text-center text-inverse">
+                       Nunca fue tan fácil encontrar algo...
+                    </p>
+
+				</form>
+				
 			</div>
-		</div>
-	</div>
+            <!-- end login-content -->
+        </div>
+        <!-- end right-container -->
+    </div>
+
 </div>
+
 @endsection

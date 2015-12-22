4c8a0296d136c681e3de011a48f32a21a1772353
Carlos Espinoza <carlosarmikhael@gmail.com>
2015-12-22 19:06:40 -0430

    lista de empresas

--- resources/views/clientes/list-empresas.blade.php
+++ resources/views/clientes/list-empresas.blade.php
@@ -1,32 +1,222 @@
-@extends('base-admin')
+@extends('base-cliente')
 
 @section('content')
 
 <div id="page-container" class="fade page-sidebar-fixed page-header-fixed">
-	
-	@include('layouts/navbar-admin')
+    
+    @include('layouts/navbar-cliente')
+    
+    <br><br>
 
-    @include('layouts/sidebar-admin')
-	
-	<div id="content" class="content ng-scope">
-        
-        <ol class="breadcrumb pull-right">
-            <div class="btn-toolbar">
-                <div class="btn-group">
-                    <a href="{{ url( '/empresas/create' ) }}" class="btn btn-success btn-sm p-l-20 p-r-20" data-toggle="tooltip" data-title="Agregar Empresas">
-                        <i class="fa fa-plus"></i>
-                    </a>
+    <section id="advertisement">
+        <div class="container">
+            <img src="{{ asset('/cart/Eshopper/images/shop/advertisement.jpg') }}" alt="">
+        </div>
+    </section>
+    
+    <section>
+        <div class="container">
+            <div class="row">
+                
+                @include('layouts/categorias-cliente')
+
+                <div class="col-sm-9 padding-right">
+                    <div class="features_items"><!--features_items-->
+                        <h2 class="title text-center">Lista de Empresas</h2>
+                        <div class="col-sm-4">
+                            <div class="product-image-wrapper">
+                                <div class="single-products">
+                                    <div class="productinfo text-center">
+                                        <a href="#">
+                                            <img src="{{ asset('/cart/Eshopper/images/shop/product12.jpg') }}" alt="">
+                                        </a>
+                                        <h5>Nombre Empresa</h5>
+                                        <p>Easy Polo Black Edition</p>
+                                        <a href="#" class="btn btn-default add-to-cart"><i class="fa fa-pencil-square-o"></i>Ver detalle</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-sm-4">
+                            <div class="product-image-wrapper">
+                                <div class="single-products">
+                                    <div class="productinfo text-center">
+                                        <a href="#">
+                                            <img src="{{ asset('/cart/Eshopper/images/shop/product11.jpg') }}" alt="">
+                                        </a>
+                                        <h5>Nombre Empresa</h5>
+                                        <p>Easy Polo Black Edition</p>
+                                        <a href="#" class="btn btn-default add-to-cart"><i class="fa fa-pencil-square-o"></i>Ver detalle</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-sm-4">
+                            <div class="product-image-wrapper">
+                                <div class="single-products">
+                                    <div class="productinfo text-center">
+                                        <a href="#">
+                                            <img src="{{ asset('/cart/Eshopper/images/shop/product10.jpg') }}" alt="">
+                                        </a>
+                                        <h5>Nombre Empresa</h5>
+                                        <p>Easy Polo Black Edition</p>
+                                        <a href="#" class="btn btn-default add-to-cart"><i class="fa fa-pencil-square-o"></i>Ver detalle</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-sm-4">
+                            <div class="product-image-wrapper">
+                                <div class="single-products">
+                                    <div class="productinfo text-center">
+                                        <a href="#">
+                                            <img src="{{ asset('/cart/Eshopper/images/shop/product9.jpg') }}" alt="">
+                                        </a>
+                                        <h5>Nombre Empresa</h5>
+                                        <p>Easy Polo Black Edition</p>
+                                        <a href="#" class="btn btn-default add-to-cart"><i class="fa fa-pencil-square-o"></i>Ver detalle</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-sm-4">
+                            <div class="product-image-wrapper">
+                                <div class="single-products">
+                                    <div class="productinfo text-center">
+                                        <a href="#">
+                                            <img src="{{ asset('/cart/Eshopper/images/shop/product8.jpg') }}" alt="">
+                                        </a>
+                                        <h5>Nombre Empresa</h5>
+                                        <p>Easy Polo Black Edition</p>
+                                        <a href="#" class="btn btn-default add-to-cart"><i class="fa fa-pencil-square-o"></i>Ver detalle</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-sm-4">
+                            <div class="product-image-wrapper">
+                                <div class="single-products">
+                                    <div class="productinfo text-center">
+                                        <a href="#">
+                                            <img src="{{ asset('/cart/Eshopper/images/shop/product7.jpg') }}" alt="">
+                                        </a>
+                                        <h5>Nombre Empresa</h5>
+                                        <p>Easy Polo Black Edition</p>
+                                        <a href="#" class="btn btn-default add-to-cart"><i class="fa fa-pencil-square-o"></i>Ver detalle</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        
+                        <div class="col-sm-4">
+                            <div class="product-image-wrapper">
+                                <div class="single-products">
+                                    <div class="productinfo text-center">
+                                        <a href="#">
+                                            <img src="{{ asset('/cart/Eshopper/images/home/product6.jpg') }}" alt="">
+                                        </a>
+                                        <h5>Nombre Empresa</h5>
+                                        <p>Easy Polo Black Edition</p>
+                                        <a href="#" class="btn btn-default add-to-cart"><i class="fa fa-pencil-square-o"></i>Ver detalle</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        
+                        <div class="col-sm-4">
+                            <div class="product-image-wrapper">
+                                <div class="single-products">
+                                    <div class="productinfo text-center">
+                                        <a href="#">
+                                            <img src="{{ asset('/cart/Eshopper/images/home/product5.jpg') }}" alt="">
+                                        </a>
+                                        <h5>Nombre Empresa</h5>
+                                        <p>Easy Polo Black Edition</p>
+                                        <a href="#" class="btn btn-default add-to-cart"><i class="fa fa-pencil-square-o"></i>Ver detalle</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        
+                        <div class="col-sm-4">
+                            <div class="product-image-wrapper">
+                                <div class="single-products">
+                                    <div class="productinfo text-center">
+                                        <a href="#">
+                                            <img src="{{ asset('/cart/Eshopper/images/home/product4.jpg') }}" alt="">
+                                        </a>
+                                        <h5>Nombre Empresa</h5>
+                                        <p>Easy Polo Black Edition</p>
+                                        <a href="#" class="btn btn-default add-to-cart"><i class="fa fa-pencil-square-o"></i>Ver detalle</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        
+                        <div class="col-sm-4">
+                            <div class="product-image-wrapper">
+                                <div class="single-products">
+                                    <div class="productinfo text-center">
+                                        <a href="#">
+                                            <img src="{{ asset('/cart/Eshopper/images/home/product3.jpg') }}" alt="">
+                                        </a>
+                                        <h5>Nombre Empresa</h5>
+                                        <p>Easy Polo Black Edition</p>
+                                        <a href="#" class="btn btn-default add-to-cart"><i class="fa fa-pencil-square-o"></i>Ver detalle</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        
+                        
+                        <div class="col-sm-4">
+                            <div class="product-image-wrapper">
+                                <div class="single-products">
+                                    <div class="productinfo text-center">
+                                        <a href="#">
+                                            <img src="{{ asset('/cart/Eshopper/images/home/product2.jpg') }}" alt="">
+                                        </a>
+                                        <h5>Nombre Empresa</h5>
+                                        <p>Easy Polo Black Edition</p>
+                                        <a href="#" class="btn btn-default add-to-cart"><i class="fa fa-pencil-square-o"></i>Ver detalle</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        
+                        <div class="col-sm-4">
+                            <div class="product-image-wrapper">
+                                <div class="single-products">
+                                    <div class="productinfo text-center">
+                                        <a href="#">
+                                            <img src="{{ asset('/cart/Eshopper/images/home/product1.jpg') }}" alt="">
+                                        </a>
+                                        <h5>Nombre Empresa</h5>
+                                        <p>Easy Polo Black Edition</p>
+                                        <a href="#" class="btn btn-default add-to-cart"><i class="fa fa-pencil-square-o"></i>Ver detalle</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        
+                        <ul class="pagination">
+                            <li class="active"><a href="">1</a></li>
+                            <li><a href="">2</a></li>
+                            <li><a href="">3</a></li>
+                            <li><a href="">»</a></li>
+                        </ul>
+                    </div><!--features_items-->
                 </div>
             </div>
-        </ol>
+        </div>
+    </section>
         
-
-        <h1 class="page-header"><i class="fa fa-laptop"></i> Lista de Todas las Empresas </h1>
+        <!--<h1 class="page-header"><i class="fa fa-laptop"></i> Lista de Todas las Empresas </h1>
         
         <div class="row">
-            <!-- begin col-12 -->
+
             <div class="col-12 ui-sortable">
-                <!-- begin panel -->
+
                 <div class="panel panel-inverse">
                     <div class="panel-heading">
                         <div class="panel-heading-btn">
@@ -59,12 +249,12 @@
 						    </tbody>
 						</table>
 	
-					</div><!-- boby -->
+					</div>
                 </div>
             </div>
         </div>
 
-    </div><!-- content -->
+    </div> -->
 	
 </div>
 @endsection

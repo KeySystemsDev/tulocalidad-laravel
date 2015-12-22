93d506c69dc4cfd9b9098c18c08e71c5bdcff4f1
Carlos Espinoza <carlosarmikhael@gmail.com>
2015-12-22 16:39:41 -0430

    carrito usuario

--- resources/views/clientes/list-carrito.blade.php
+++ resources/views/clientes/list-carrito.blade.php
@@ -1,27 +1,125 @@
-@extends('base-admin')
+@extends('base-cliente')
 
 @section('content')
 
 <div id="page-container" class="fade page-sidebar-fixed page-header-fixed">
 	
-	@include('layouts/navbar-admin')
+	@include('layouts/navbar-cliente')
 
-    @include('layouts/sidebar-admin')
+	<br><br>
+
+	<section id="advertisement">
+		<div class="container">
+			<img src="{{ asset('/cart/Eshopper/images/shop/advertisement.jpg') }}" alt="" />
+		</div>
+	</section>
+
+	<section id="cart_items">
+		<div class="container">
+			<div class="cart_info">
+				<table class="table table-condensed">
+					<thead>
+						<tr class="cart_menu">
+							<td class="image">Item</td>
+							<td class="description"></td>
+							<td class="price">Price</td>
+							<td class="total">Total</td>
+							<td></td>
+						</tr>
+					</thead>
+					<tbody>
+						<tr>
+							<td class="cart_product">
+								<a href=""><img src="{{ asset('/cart/Eshopper/images/cart/one.png') }}" alt=""></a>
+							</td>
+							<td class="cart_description">
+								<h4><a href="">Colorblock Scuba</a></h4>
+								<p>Web ID: 1089772</p>
+							</td>
+							<td class="cart_price">
+								<p>$59</p>
+							</td>
+							<td class="cart_total">
+								<p class="cart_total_price">$59</p>
+							</td>
+							<td class="cart_delete">
+								<a class="cart_quantity_delete" href=""><i class="fa fa-times"></i></a>
+							</td>
+						</tr>
+
+						<tr>
+							<td class="cart_product">
+								<a href=""><img src="{{ asset('/cart/Eshopper/images/cart/two.png') }}" alt=""></a>
+							</td>
+							<td class="cart_description">
+								<h4><a href="">Colorblock Scuba</a></h4>
+								<p>Web ID: 1089772</p>
+							</td>
+							<td class="cart_price">
+								<p>$59</p>
+							</td>
+							<td class="cart_total">
+								<p class="cart_total_price">$59</p>
+							</td>
+							<td class="cart_delete">
+								<a class="cart_quantity_delete" href=""><i class="fa fa-times"></i></a>
+							</td>
+						</tr>
+						<tr>
+							<td class="cart_product">
+								<a href=""><img src="{{ asset('/cart/Eshopper/images/cart/three.png') }}" alt=""></a>
+							</td>
+							<td class="cart_description">
+								<h4><a href="">Colorblock Scuba</a></h4>
+								<p>Web ID: 1089772</p>
+							</td>
+							<td class="cart_price">
+								<p>$59</p>
+							</td>
+							<td class="cart_total">
+								<p class="cart_total_price">$59</p>
+							</td>
+							<td class="cart_delete">
+								<a class="cart_quantity_delete" href=""><i class="fa fa-times"></i></a>
+							</td>
+						</tr>
+					</tbody>
+				</table>
+			</div>
+		</div>
+	</section> <!--/#cart_items-->
+
+	<section id="do_action">
+		<div class="container">
+			<div class="heading">
+				<h3>What would you like to do next?</h3>
+				<p>Choose if you have a discount code or reward points you want to use or would like to estimate your delivery cost.</p>
+			</div>
+			<div class="row">
+				
+				<div class="col-sm-6">
+					<div class="total_area">
+						<ul>
+							<li>Cart Sub Total <span>$59</span></li>
+							<li>Eco Tax <span>$2</span></li>
+							<li>Shipping Cost <span>Free</span></li>
+							<li>Total <span>$61</span></li>
+						</ul>
+							<a class="btn btn-info update" href="">Comprar <i class="fa fa fa-shopping-cart"></i></a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
 	
-	<div id="content" class="content ng-scope">
+	<!--<div id="content" class="content ng-scope">
         
-        <ol class="breadcrumb pull-right">
-            <div class="btn-toolbar">
-            </div>
-        </ol>
-        
-
         <h1 class="page-header"><i class="fa fa-laptop"></i> Lista de todos los productos </h1>
         
         <div class="row">
-            <!-- begin col-12 -->
+
             <div class="col-12 ui-sortable">
-                <!-- begin panel -->
+
                 <div class="panel panel-inverse">
                     <div class="panel-heading">
                         <div class="panel-heading-btn">
@@ -58,7 +156,7 @@
 						    </tbody>
 						</table>
 
-					</div><!-- boby -->
+					</div>
 
 
                 </div>
@@ -74,7 +172,9 @@
             </div>
         </div>
 
-    </div><!-- content -->
+    </div> -->
+
+	@include('layouts/footer-cliente')
 	
 </div>
 @endsection

048cb5d44b9ef56526b410e7fba1c0d8788e79c9
Homero Hernandez <hsh283@gmail.com>
2016-01-10 18:59:52 -0430

    cambiando tipo de atributos

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -190,11 +190,11 @@ class ClientController extends Controller {
 			
 			$articulo_data = [
 					'title' => $articulo['nombre_empresa'],
-					'quantity' =>(int) $articulo->cantidad_producto_carrito,
+					'quantity' =>intval($articulo->cantidad_producto_carrito),
 					'description' =>'asd',
-					'picture_url' =>'https://test-tulocalidad/uploads/empresas/high/'.$articulo['url_imagen_empresa'],
+					//'picture_url' =>url('/uploads/empresas/high/'.$articulo['url_imagen_empresa']),
 					'currency_id'=> 'VEF',
-					'unit_price'=>(float) $articulo['data_producto']->precio_producto
+					'unit_price'=> $articulo['data_producto']->precio_producto
 			];
 			array_push($preference_data['items'],$articulo_data);
 		};

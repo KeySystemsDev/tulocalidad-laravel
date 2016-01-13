e632dcf1cbe3e08ac93d897dac6c3ea3f88a24a6
Homero Hernandez <hsh283@gmail.com>
2016-01-13 14:41:31 -0430

    probando htaccess

--- app/Http/Controllers/ApisController.php
+++ app/Http/Controllers/ApisController.php
@@ -17,28 +17,28 @@ class ApisController extends Controller {
 	}
 
 
-	public function filter(Request $request){
-		    // header('Access-Control-Allow-Origin: *');
-		    // header('Access-Control-Allow-Methods: GET, POST');
-		    // header('Access-Control-Allow-Headers: Origin, Content-Type, Accept, Authorization, X-Request-With');
-		    // header('Access-Control-Allow-Credentials: true');
+	public function filter(){
+		    header('Access-Control-Allow-Origin: *');
+		    header('Access-Control-Allow-Methods: GET, POST');
+		    header('Access-Control-Allow-Headers: Origin, Content-Type, Accept, Authorization, X-Request-With');
+		    header('Access-Control-Allow-Credentials: true');
 
-				   header("Access-Control-Allow-Origin: *");
+				//    header("Access-Control-Allow-Origin: *");
 
-		  // ALLOW OPTIONS METHOD
-		  $headers = [
-		         'Access-Control-Allow-Methods'=> 'POST, GET, OPTIONS, PUT, DELETE',
-		         'Access-Control-Allow-Headers'=> 'Content-Type, X-Auth-Token, Origin'
-		     ];
-		  if($request->getMethod() == "OPTIONS") {
-		         // The client-side application can set only headers allowed in Access-Control-Allow-Headers
-		         return Response::make('OK', 200, $headers);
-		     }
+		  // // ALLOW OPTIONS METHOD
+		  // $headers = [
+		  //        'Access-Control-Allow-Methods'=> 'POST, GET, OPTIONS, PUT, DELETE',
+		  //        'Access-Control-Allow-Headers'=> 'Content-Type, X-Auth-Token, Origin'
+		  //    ];
+		  // if($request->getMethod() == "OPTIONS") {
+		  //        // The client-side application can set only headers allowed in Access-Control-Allow-Headers
+		  //        return Response::make('OK', 200, $headers);
+		  //    }
 
-		     $response = $next($request);
-		     foreach($headers as $key => $value) 
-		      $response->header($key, $value);
-		  return $response;
+		  //    $response = $next($request);
+		  //    foreach($headers as $key => $value) 
+		  //     $response->header($key, $value);
+		  // return $response;
 
 	}
 

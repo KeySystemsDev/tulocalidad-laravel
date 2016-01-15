28396aa8808adebb366f7ccbf8763b7d92abbe5e
Carlos Espinoza <carlosarmikhael@gmail.com>
2016-01-15 15:53:58 -0430

    vista de volver

--- resources/views/clientes/volver.blade.php
+++ resources/views/clientes/volver.blade.php
@@ -1,1 +1,19 @@
-vuelva a su aplicacion
+@extends('base-admin')
+
+@section('content')
+
+<div id="page-container" class="fade page-sidebar-fixed page-header-fixed">
+
+	 <section id="do_action">
+		<div class="row">
+			<div class="col-md-12 cart-none">
+				<i class="fa fa-shopping-cart"></i>
+				<h1> Su compra fue realizada exitozamente.</h1>
+				<p>Vuelva a su App</p>
+			</div>
+		</div>
+	</section>
+   
+	
+</div>
+@endsection

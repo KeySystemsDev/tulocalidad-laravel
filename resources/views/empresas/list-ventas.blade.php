d50f039478a152e5633a1cb8b01b6951a5d863f7
Carlos Espinoza <carlosarmikhael@gmail.com>
2016-01-15 14:31:51 -0430

    list ventas

--- resources/views/empresas/list-ventas.blade.php
+++ resources/views/empresas/list-ventas.blade.php
@@ -20,14 +20,16 @@
 	    </ol>
 
 	    <h1 class="page-header page-header-new">.</h1>
-		
+
+		@if(count($ventas)!=0)
+
+		@foreach($ventas as $compra)
 	    <section id="cart_items">
-			
 			<div class="table-responsive cart_info">
 				<table class="table table-condensed">
 					<thead>
 						<tr class="cart_menu">
-							<td class="image">Carrito</td>
+							<td class="image">Articulos</td>
 							<td class="description"></td>
 							<td class="price">Precio</td>
 							<td class="quantity">Cantidad</td>
@@ -35,50 +37,32 @@
 						</tr>
 					</thead>
 					<tbody>
+						@foreach($compra['productos_comprados'] as $articulo)
 						<tr>
 							<td class="cart_product">
-								<a href=""><img src="{{ url('cart/Eshopper/images/cart/one.png')}}" alt=""></a>
+								<a href=""><img src="{{ url('/uploads/productos/low/'.$articulo['primera_imagen']) }}" alt=""></a>
 							</td>
 							<td class="cart_description">
-								<h4><a href="">Colorblock Scuba</a></h4>
-								<p>Web ID: 1089772</p>
+								<h4><a href="">{{$articulo['nombre_producto']}}</a></h4>
+								<p>Web ID: {{$articulo['id_producto_comprado']}}</p>
 							</td>
 							<td class="cart_price">
-								<p>$59</p>
+								<p>{{$articulo['precio_unidad']}} BsF</p>
 							</td>
 							<td class="cart_quantity">
-								2
+								{{$articulo['cantidad_producto_comprados']}}
 							</td>
 							<td class="cart_total">
-								<p class="cart_total_price">100 BsF</p>
+								<p class="cart_total_price">{{$articulo['precio_total']}} BsF</p>
 							</td>
 						</tr>
-						<tr>
-							<td class="cart_product">
-								<a href=""><img src="{{ url('cart/Eshopper/images/cart/one.png')}}" alt=""></a>
-							</td>
-							<td class="cart_description">
-								<h4><a href="">Colorblock Scuba</a></h4>
-								<p>Web ID: 1089772</p>
-							</td>
-							<td class="cart_price">
-								<p>$59</p>
-							</td>
-							<td class="cart_quantity">
-								2
-							</td>
-							<td class="cart_total">
-								<p class="cart_total_price">100 BsF</p>
-							</td>
-						</tr>
+						@endforeach
 					</tbody>
 				</table>
 			</div>
-			
 		</section>
 
 		<section id="do_action">
-		
 			<!--<div class="heading">
 				<h3>Seguro que desea comprar los productos?</h3>
 				<p>Todos los productos seran borrados de su carrito de compra una vez efectue la operación.</p>
@@ -88,27 +72,24 @@
 				<div class="col-sm-6">
 					<div class="total_area">
 						<ul>
-							<li>Empresa <span>empresa</span></li>
-							<li>Sub Total<span>100 BsF</span></li>
-							<li>Total <span>200 BsF</span></li>
+							<li>Tipo Pago <span>{{$compra['tipo_pago_compra']}}</span></li>
+							<li>Factura<span>{{$compra['identificador_pago_compra']}}</span></li>
+							<li>Total <span>{{$compra['precio_total_compra']}} BsF</span></li>
 						</ul>
 					</div>
 				</div>
 			</div>
-			
 		</section>
-		
+	@endforeach
+	@else
 		<section id="do_action">
-			
 			<div class="row">
 				<div class="col-md-12 cart-none">
 					<i class="fa fa-usd"></i>
 					<h1> No tiene Ventas Realizadas.</h1>
 				</div>
 			</div>
-			
 		</section>
-
-	</div>
-
+	@endif
+	</div>	
 </div>

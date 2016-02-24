317df3b333c8fb3db23b2cf4bf5e0b0552a798a4
Homero Hernandez <hsh283@gmail.com>
2016-02-24 16:44:26 -0430

    pruebas de ipn

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -361,8 +361,9 @@ class ClientController extends Controller {
 
 	public function IPNotificador(Request $request){
 
-		$mp = new MP(env('MP_APP_ID'), env("MP_APP_SECRET"));
+		$mp = new MP(env('MP_APP_ID',''), env('MP_APP_SECRET', ''));
 
+		HelperController::sendEmail("hsh283@gmail.com","homero Hernandez",'prueba', 'emails.prueba', ['response'=>$request]);
 
 		// Get the payment and the corresponding merchant_order reported by the IPN.
 		if($request->topic == 'payment'){
@@ -372,30 +373,30 @@ class ClientController extends Controller {
 		} else if($request->topic == 'merchant_order'){
 			$merchant_order_info = $mp->get("/merchant_orders/" . $request->id);
 		}
+
 		HelperController::sendEmail("hsh283@gmail.com","homero Hernandez",'prueba', 'emails.prueba', ['response'=>$merchant_order_info]);
+		// if ($merchant_order_info["status"] == 200) {
+		// 	// If the payment's transaction amount is equal (or bigger) than the merchant_order's amount you can release your items 
+		// 	$paid_amount = 0;
 
-		if ($merchant_order_info["status"] == 200) {
-			// If the payment's transaction amount is equal (or bigger) than the merchant_order's amount you can release your items 
-			$paid_amount = 0;
+		// 	foreach ($merchant_order_info["response"]["payments"] as  $payment) {
+		// 		if ($payment['status'] == 'approved'){
+		// 			$paid_amount += $payment['transaction_amount'];
+		// 		}	
+		// 	}
 
-			foreach ($merchant_order_info["response"]["payments"] as  $payment) {
-				if ($payment['status'] == 'approved'){
-					$paid_amount += $payment['transaction_amount'];
-				}	
-			}
-
-			if($paid_amount >= $merchant_order_info["response"]["total_amount"]){
-				if(count($merchant_order_info["response"]["shipments"]) > 0) { // The merchant_order has shipments
-					if($merchant_order_info["response"]["shipments"][0]["status"] == "ready_to_ship"){
-						print_r("Totally paid. Print the label and release your item.");
-					}
-				} else { // The merchant_order don't has any shipments
-					print_r("Totally paid. Release your item.");
-				}
-			} else {
-				print_r("Not paid yet. Do not release your item.");
-			}
-		}
+		// 	if($paid_amount >= $merchant_order_info["response"]["total_amount"]){
+		// 		if(count($merchant_order_info["response"]["shipments"]) > 0) { // The merchant_order has shipments
+		// 			if($merchant_order_info["response"]["shipments"][0]["status"] == "ready_to_ship"){
+		// 				print_r("Totally paid. Print the label and release your item.");
+		// 			}
+		// 		} else { // The merchant_order don't has any shipments
+		// 			print_r("Totally paid. Release your item.");
+		// 		}
+		// 	} else {
+		// 		print_r("Not paid yet. Do not release your item.");
+		// 	}
+		// }
 
 
 
